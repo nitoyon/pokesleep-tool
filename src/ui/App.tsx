c396b0e6f59dc0aa08490c6c98a3596885f3b40e
nitoyon <nitoyon@gmail.com>
2024-03-10 13:53:26 +0900

    Refactor: extract main preview panel into GeneralPanel.tsx

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,12 +1,10 @@
 import './App.css';
 import React, { useCallback, useEffect, useState } from 'react';
 import { InputArea, InputAreaData } from './InputArea';
-import BetterSecondSleepDialog, { BetterSecondSleepData } from './Dialog/BetterSecondSleepDialog';
-import PreviewScore, {getScoreRange} from './PreviewScore';
+import GeneralPanel from './GeneralPanel';
 import PwaNotify from './PwaBanner';
 import fields from '../data/fields';
 import { useTranslation } from 'react-i18next'
-import PreviewEncounter from './PreviewEncounter';
 
 interface AppConfig extends InputAreaData {
     /** current language */
@@ -85,20 +83,6 @@ export default function App({config}: {config:AppConfig}) {
     }, [language, t]);
     useEffect(updateHtml, [updateHtml, language, i18n, t]);
 
-    const [isBetterSecondSleepDialogOpen, setBetterSecondSleepOpen] = useState(false);
-    const onBetterSecondSleepDialogClose = () => {
-        setBetterSecondSleepOpen(false);
-    };
-
-    const [betterSecondSleepData, setBetterSecondSleepData] = useState<BetterSecondSleepData>({
-        first: {count: 0, score: 0, strength: 0},
-        second: {count: 0, score: 0, strength: 0},
-    });
-    function onSecondSleepDetailClick(data:BetterSecondSleepData) {
-        setBetterSecondSleepData(data);
-        setBetterSecondSleepOpen(true);
-    }
-
     const onPwaBannerClose = useCallback(() => {
         config.pwacnt = 0;
         saveConfig(config);
@@ -107,22 +91,8 @@ export default function App({config}: {config:AppConfig}) {
     return (
         <div className="content">
             <InputArea data={data} onChange={onChange}/>
-            <div className="preview">
-                <PreviewEncounter data={data}/>
-                <PreviewScore count={4} data={data} ranges={getScoreRange(4, data)}
-                    onSecondSleepDetailClick={onSecondSleepDetailClick}/>
-                <PreviewScore count={5} data={data} ranges={getScoreRange(5, data)}
-                    onSecondSleepDetailClick={onSecondSleepDetailClick}/>
-                <PreviewScore count={6} data={data} ranges={getScoreRange(6, data)}
-                    onSecondSleepDetailClick={onSecondSleepDetailClick}/>
-                <PreviewScore count={7} data={data} ranges={getScoreRange(7, data)}
-                    onSecondSleepDetailClick={onSecondSleepDetailClick}/>
-                <PreviewScore count={8} data={data} ranges={getScoreRange(8, data)}
-                    onSecondSleepDetailClick={onSecondSleepDetailClick}/>
-            </div>
+            <GeneralPanel data={data}/>
             <PwaNotify pwaCount={config.pwacnt} onClose={onPwaBannerClose}/>
-            <BetterSecondSleepDialog data={betterSecondSleepData}
-                open={isBetterSecondSleepDialogOpen} onClose={onBetterSecondSleepDialogClose}/>
         </div>
     );
 }
---
+++ src/ui/GeneralPanel.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { InputAreaData } from './InputArea';
+import BetterSecondSleepDialog, { BetterSecondSleepData } from './Dialog/BetterSecondSleepDialog';
+import PreviewScore, {getScoreRange} from './PreviewScore';
+import PreviewEncounter from './PreviewEncounter';
+
+export default function GeneralPanel({data}: {data: InputAreaData}) {
+    const [isBetterSecondSleepDialogOpen, setBetterSecondSleepOpen] = useState(false);
+    const onBetterSecondSleepDialogClose = () => {
+        setBetterSecondSleepOpen(false);
+    };
+
+    const [betterSecondSleepData, setBetterSecondSleepData] = useState<BetterSecondSleepData>({
+        first: {count: 0, score: 0, strength: 0},
+        second: {count: 0, score: 0, strength: 0},
+    });
+    function onSecondSleepDetailClick(data:BetterSecondSleepData) {
+        setBetterSecondSleepData(data);
+        setBetterSecondSleepOpen(true);
+    }
+
+    return (
+        <div className="preview">
+            <PreviewEncounter data={data}/>
+            <PreviewScore count={4} data={data} ranges={getScoreRange(4, data)}
+                onSecondSleepDetailClick={onSecondSleepDetailClick}/>
+            <PreviewScore count={5} data={data} ranges={getScoreRange(5, data)}
+                onSecondSleepDetailClick={onSecondSleepDetailClick}/>
+            <PreviewScore count={6} data={data} ranges={getScoreRange(6, data)}
+                onSecondSleepDetailClick={onSecondSleepDetailClick}/>
+            <PreviewScore count={7} data={data} ranges={getScoreRange(7, data)}
+                onSecondSleepDetailClick={onSecondSleepDetailClick}/>
+            <PreviewScore count={8} data={data} ranges={getScoreRange(8, data)}
+                onSecondSleepDetailClick={onSecondSleepDetailClick}/>
+            <BetterSecondSleepDialog data={betterSecondSleepData}
+                open={isBetterSecondSleepDialogOpen} onClose={onBetterSecondSleepDialogClose}/>
+        </div>
+    );
+}
