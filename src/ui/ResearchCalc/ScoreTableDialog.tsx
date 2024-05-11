e1132c105ff3e4616db7f35f617a948d94e7b2f6
nitoyon <nitoyon@gmail.com>
2024-05-12 00:30:25 +0900

    ResearchCalc: Add drowsy power to sleep score table

--- src/ui/ResearchCalc/ScoreTableDialog.tsx
+++ src/ui/ResearchCalc/ScoreTableDialog.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { styled } from '@mui/system';
-import { Button, Dialog, DialogActions, DialogTitle, DialogContent } from '@mui/material';
+import { Button, Dialog, DialogActions, DialogTitle,
+    ToggleButtonGroup, ToggleButton } from '@mui/material';
 import SleepScore from '../SleepScore';
 import {getMinTimeForScore, getMaxTimeForScore} from './PreviewScore';
 import { useTranslation } from 'react-i18next';
@@ -10,45 +11,88 @@ interface ScoreTableDialogProps {
     open: boolean;
     /** callback function when dialog is closed */
     onClose: () => void;
+    /** Current strength.  */
+    strength: number;
 }
 
-export default function ScoreTableDialog({open, onClose}: ScoreTableDialogProps) {
+const ScoreTableDialog = React.memo(({open, onClose, strength}: ScoreTableDialogProps) => {
     const { t } = useTranslation();
+    const ref0 = React.useRef<HTMLDivElement|null>(null);
+    const ref25 = React.useRef<HTMLDivElement|null>(null);
+    const ref50 = React.useRef<HTMLDivElement|null>(null);
+    const ref75 = React.useRef<HTMLDivElement|null>(null);
+    const ref100 = React.useRef<HTMLDivElement|null>(null);
+
+    const onMoveClick = React.useCallback((e: any, val: number|null) => {
+        if (val === null) { return; }
+        let element: HTMLDivElement|null = null;
+        switch (val) {
+            case 0: element = ref0?.current; break;
+            case 25: element = ref25?.current; break;
+            case 50: element = ref50?.current; break;
+            case 75: element = ref75?.current; break;
+            case 100: element = ref100?.current; break;
+        }
+        if (element !== null) {
+            element.scrollIntoView({behavior: 'smooth', block: 'start'});
+        }
+    }, []);
+
+    if (!open) {
+        return <></>;
+    }
 
     const scores = [];
     for (let i = 0; i <= 100; i++) {
-        scores.push(<React.Fragment key={i}>
+        scores.push(<div className="sleep_time" key={i}
+            ref={i === 0 ? ref0 : i === 25 ? ref25 : i === 50 ? ref50 : 
+                i === 75 ? ref75 : i === 100 ? ref100 : null}>
             <SleepScore score={i}/>
-            <span>{getMinTimeForScore(i).toString(t)}</span>
-            <span>{t('range separator')}</span>
-            <span>{getMaxTimeForScore(i).toString(t)}</span>
-        </React.Fragment>);
+            <div className="time">
+                {getMinTimeForScore(i).toString(t)}
+                <> {t('range separator')} </>
+                {getMaxTimeForScore(i).toString(t)}
+                <div className="time_power">{t('num', {n: i * strength})}</div>
+            </div>
+        </div>);
     }
 
     return (
         <StyledDialog open={open} onClose={onClose} scroll="paper">
             <DialogTitle>{t('sleep score table')}</DialogTitle>
-            <DialogContent dividers>
-                <div className="sleep_score_table">
-                    {scores}
-                </div>
-            </DialogContent>
+            <div className="sleep_score_table">
+                {scores}
+            </div>
+            <div className="form">
+                <ToggleButtonGroup onChange={onMoveClick} exclusive>
+                    <ToggleButton value={0}>0</ToggleButton>
+                    <ToggleButton value={25}>25</ToggleButton>
+                    <ToggleButton value={50}>50</ToggleButton>
+                    <ToggleButton value={75}>75</ToggleButton>
+                    <ToggleButton value={100}>100</ToggleButton>
+                </ToggleButtonGroup>
+            </div>
             <DialogActions>
                 <Button onClick={onClose}>{t('close')}</Button>
             </DialogActions>
         </StyledDialog>
     );
-}
+});
+
+export default ScoreTableDialog;
 
 const StyledDialog = styled(Dialog)({
+    '& div.form': {
+        margin: '.4rem 1.2rem 0 1.2rem',
+        '& > div > button': {
+            width: '3rem',
+        },
+    },
     '& div.sleep_score_table': {
-        display: 'grid',
-        gridTemplateColumns: 'auto auto auto auto',
-        rowGap: '.5rem',
-        columnGap: '.3rem',
-        '& > span': {
-            fontSize: '.9rem',
-            paddingTop: '.4rem',
+        overflow: 'scroll',
+        padding: '0 1rem',
+        '& > div.sleep_time': {
+            margin: '.4rem 0',
         },
     },    
 });
