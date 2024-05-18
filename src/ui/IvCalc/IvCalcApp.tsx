08656567a8a9c2a67c19a0059134653faae108fa
nitoyon <nitoyon@gmail.com>
2024-05-18 22:55:53 +0900

    IV: Lift up CalculateParameter state from StrengthView to IvCalcApp

--- src/ui/IvCalc/IvCalcApp.tsx
+++ src/ui/IvCalc/IvCalcApp.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { styled } from '@mui/system';
 import { Tab, Tabs } from '@mui/material'; 
 import PokemonIv from '../../util/PokemonIv';
+import { CalculateParameter, loadCalculateParameter } from '../../util/PokemonStrength';
 import LowerTabView from './LowerTabView';
 import RpView from './RpView';
 import StrengthView from './StrengthView';
@@ -17,17 +18,40 @@ export const StyledTab = styled(Tab)({
     padding: '6px 16px',
 });
 
+const defaultCalculateParameter = loadCalculateParameter();
+
 const ResearchCalcApp = React.memo(() => {
     const [tabIndex, setTabIndex] = React.useState(0);
     const [pokemonIv, setPokemonIv] = React.useState(new PokemonIv("Venusaur"));
+    const [parameter, setParameter] = React.useState(defaultCalculateParameter);
     const { t } = useTranslation();
     const width = useDomWidth();
-
-    pokemonIv.normalize();
 
     const onTabChange = React.useCallback((event: React.SyntheticEvent, newValue: number) => {
         setTabIndex(newValue);
     }, [setTabIndex]);
+
+    const onParameterChange = React.useCallback((value: CalculateParameter) => {
+        setParameter(value);
+        localStorage.setItem('PstStrenghParam', JSON.stringify(value));
+    }, [setParameter]);
+
+    const onPokemonIvChange = React.useCallback((value: PokemonIv) => {
+        // fix helpingBonusCount
+        const hasHelpingBonus = value.hasHelpingBonusInActiveSubSkills;
+        const prevHasHelpingBonus = pokemonIv.hasHelpingBonusInActiveSubSkills;
+        if (hasHelpingBonus && parameter.helpBonusCount === 0) {
+            onParameterChange({...parameter, helpBonusCount: 1});
+        } else if (parameter.helpBonusCount === 1 && !hasHelpingBonus &&
+            prevHasHelpingBonus) {
+            onParameterChange({...parameter, helpBonusCount: 0});
+        } else if (!hasHelpingBonus && parameter.helpBonusCount === 5) {
+            onParameterChange({...parameter, helpBonusCount: 4});
+        }
+
+        value.normalize();
+        setPokemonIv(value);
+    }, [parameter, pokemonIv, setPokemonIv, onParameterChange]);
 
     return <div style={{margin: "0 .5rem 10rem"}}>
         <StyledTabs value={tabIndex} onChange={onTabChange}>
@@ -36,9 +60,10 @@ const ResearchCalcApp = React.memo(() => {
             <StyledTab label={t('rating')}/>
         </StyledTabs>
         {tabIndex === 0 && <RpView pokemonIv={pokemonIv} width={width}/>}
-        {tabIndex === 1 && <StrengthView pokemonIv={pokemonIv}/>}
+        {tabIndex === 1 && <StrengthView pokemonIv={pokemonIv}
+            parameter={parameter} onParameterChange={onParameterChange}/>}
         {tabIndex === 2 && <RatingView pokemonIv={pokemonIv} width={width}/>}
-        <LowerTabView pokemonIv={pokemonIv} onChange={setPokemonIv}/>
+        <LowerTabView pokemonIv={pokemonIv} onChange={onPokemonIvChange}/>
     </div>;
 });
 
--- src/ui/IvCalc/StrengthView.tsx
+++ src/ui/IvCalc/StrengthView.tsx
@@ -1,40 +1,20 @@
 import React from 'react';
 import PokemonIv from '../../util/PokemonIv';
-import { CalculateParameter, loadCalculateParameter } from '../../util/PokemonStrength';
+import { CalculateParameter } from '../../util/PokemonStrength';
 import StrengthBerryIngSkillView from './StrengthBerryIngSkillView';
 import StrengthSettingForm from './StrengthSettingForm';
 
 export type PeriodType = "1day"|"1week"|"whistle";
 
-const StrengthView = React.memo(({pokemonIv}: {pokemonIv: PokemonIv}) => {
-    let [settings, setAdvancedSettings] = React.useState<CalculateParameter|null>(null);
-    if (settings === null) {
-        settings = loadCalculateParameter();
-    }
-
-    const onAdvancedSettingsChange = React.useCallback((value: CalculateParameter) => {
-        setAdvancedSettings(value);
-        localStorage.setItem('PstStrenghParam', JSON.stringify(value));
-    }, [setAdvancedSettings]);
-
-    // fix helpingBonusCount
+const StrengthView = React.memo(({pokemonIv, parameter, onParameterChange}: {
+    pokemonIv: PokemonIv,
+    parameter: CalculateParameter,
+    onParameterChange: (value: CalculateParameter) => void,
+}) => {
     const hasHelpingBonus = pokemonIv.hasHelpingBonusInActiveSubSkills;
-    const prevHasHelpingBonus = React.useRef<boolean>(hasHelpingBonus);
-    React.useEffect(() => {
-        prevHasHelpingBonus.current = hasHelpingBonus;
-    }, [hasHelpingBonus]);
-    if (hasHelpingBonus && settings.helpBonusCount === 0) {
-        settings.helpBonusCount = 1;
-    } else if (settings.helpBonusCount === 1 && !hasHelpingBonus &&
-        prevHasHelpingBonus.current) {
-        settings.helpBonusCount = 0;
-    } else if (!hasHelpingBonus && settings.helpBonusCount === 5) {
-        settings.helpBonusCount = 4;
-    }
-
     return (<div>
-        <StrengthBerryIngSkillView pokemonIv={pokemonIv} settings={settings}/>
-        <StrengthSettingForm value={settings} onChange={onAdvancedSettingsChange}
+        <StrengthBerryIngSkillView pokemonIv={pokemonIv} settings={parameter}/>
+        <StrengthSettingForm value={parameter} onChange={onParameterChange}
             hasHelpingBonus={hasHelpingBonus}/>
     </div>);
 });
