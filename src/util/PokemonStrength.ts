a40f2ff884368490b02e22c1ce0efe29fbde5c3d
nitoyon <nitoyon@gmail.com>
2024-05-18 22:31:41 +0900

    IV: extract loadCalculateParameter from StrengthView to PokemonStrength

--- src/util/PokemonStrength.ts
+++ src/util/PokemonStrength.ts
@@ -245,4 +245,69 @@ class PokemonStrength {
     }
 }
 
+/**
+ * Load CalculateParameter fron localStorage.
+ * @returns Loaded parameter.
+ */
+export function loadCalculateParameter(): CalculateParameter {
+    const ret: CalculateParameter = {
+        period: 24,
+        fieldBonus: 0,
+        favorite: false,
+        helpBonusCount: 0,
+        isGoodCampTicketSet: false,
+        averageEfficiency: 1.8452,
+        tapFrequency: "always",
+        recipeBonus: 25,
+        recipeLevel: 30,
+        event: "none",
+    };
+
+    const settings = localStorage.getItem('PstStrenghParam');
+    if (settings === null) {
+        return ret;
+    }
+    const json = JSON.parse(settings);
+    if (typeof(json) !== "object" || json === null) {
+        return ret;
+    }
+    if (typeof(json.period) === "number" &&
+        [24, 168, 3].includes(json.period)) {
+        ret.period = json.period;
+    }
+    if (typeof(json.fieldBonus) === "number" &&
+        Math.floor(json.fieldBonus / 5) === json.fieldBonus / 5 &&
+        json.fieldBonus >= 0 && json.fieldBonus <= 60) {
+        ret.fieldBonus = json.fieldBonus;
+    }
+    if (typeof(json.favorite) === "boolean") {
+        ret.favorite = json.favorite;
+    }
+    if (typeof(json.helpBonusCount) === "number" &&
+        Math.floor(json.helpBonusCount) === json.helpBonusCount &&
+        json.helpBonusCount >= 0 && json.helpBonusCount <= 4) {
+        ret.helpBonusCount = json.helpBonusCount;
+    }
+    if (typeof(json.isGoodCampTicketSet) === "boolean") {
+        ret.isGoodCampTicketSet = json.isGoodCampTicketSet;
+    }
+    if (typeof(json.tapFrequency) === "string" &&
+        ["always", "none"].includes(json.tapFrequency)) {
+        ret.tapFrequency = json.tapFrequency;
+    }
+    if (typeof(json.recipeBonus) === "number" &&
+        [0, 6, 11, 17, 25, 35, 48].includes(json.recipeBonus)) {
+        ret.recipeBonus = json.recipeBonus;
+    }
+    if (typeof(json.recipeLevel) === "number" &&
+        [1, 10, 20, 30, 40, 50, 55].includes(json.recipeLevel)) {
+        ret.recipeLevel = json.recipeLevel;
+    }
+    if (typeof(json.event) === "string" &&
+        ["none", "entei 1st week", "entei 2nd week"].includes(json.event)) {
+        ret.event = json.event;
+    }
+    return ret;
+}
+
 export default PokemonStrength;
