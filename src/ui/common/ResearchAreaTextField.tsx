bd0091e0d591dd4846df4e61144ef8e6c020442a
nitoyon <nitoyon@gmail.com>
2024-05-19 16:03:20 +0900

    ResearchCalc: Extract ResearchAreaTextField from InputArea to common

---
+++ src/ui/common/ResearchAreaTextField.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import fields from '../../data/fields';
+import { MenuItem, TextField } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+
+const ResearchAreaTextField = React.memo(({value, onChange}:{
+    value: number;
+    onChange: (value: number) => void;
+}) => {
+    const { t } = useTranslation();
+    const onChangeHandler = React.useCallback((e: any) => {
+        onChange(e.target.value as number);
+    }, [onChange]);
+
+    // prepare field menus
+    const fieldMenuItems = [];
+    for (const field of fields) {
+        fieldMenuItems.push(
+            <MenuItem key={field.index} value={field.index}>
+                <span className="field_icon">{field.emoji}</span>
+                {t(`area.${field.index}`)}
+            </MenuItem>
+        );
+    }
+
+    return (
+        <TextField variant="standard" size="small" select value={value}
+            SelectProps={{ MenuProps: {
+                anchorOrigin: { vertical: "bottom", horizontal: "left" },
+                transformOrigin: { vertical: "top", horizontal: "left" },
+            }}}
+            onChange={onChangeHandler}>
+            {fieldMenuItems}
+        </TextField>
+    );
+});
+
+export default ResearchAreaTextField;
