78626b8a27552a10105a21b48c809524f3e04c14
nitoyon <nitoyon@gmail.com>
2024-05-21 22:28:17 +0900

    IV: Extract PokemonFilterFooter from PokemonSelectDialog

--- src/ui/IvCalc/PokemonSelectDialog.tsx
+++ src/ui/IvCalc/PokemonSelectDialog.tsx
@@ -2,15 +2,14 @@ import React, { useCallback } from 'react';
 import { styled } from '@mui/system';
 import { PokemonType, PokemonTypes } from '../../data/pokemons';
 import { Autocomplete, autocompleteClasses, AutocompleteRenderGroupParams,
-    Button, ButtonBase, ClickAwayListener, Dialog, DialogActions,
+    Button, Dialog, DialogActions,
     FilterOptionsState, InputAdornment, InputBase,
-    MenuItem, MenuList, Popper, Paper, ToggleButton, ToggleButtonGroup } from '@mui/material';
+    MenuItem, ToggleButton, ToggleButtonGroup } from '@mui/material';
 import PokemonIcon from './PokemonIcon';
+import PokemonFilterFooter, { PokemonFilterConfig } from './PokemonFilterFooter';
 import { PokemonOption } from './PokemonTextField';
 import CheckIcon from '@mui/icons-material/Check';
 import SearchIcon from '@mui/icons-material/Search';
-import SortIcon from '@mui/icons-material/Sort';
-import NorthIcon from '@mui/icons-material/North';
 import { useTranslation } from 'react-i18next';
 
 const StyledDialog = styled(Dialog)({
@@ -93,11 +92,6 @@ const StyledInput = styled(InputBase)(({ theme }) => ({
     },
 }));
 
-const PokemonSelectDialogFooter = styled('div')({
-    backgroundColor: '#f76',
-    padding: '.5rem .5rem',
-});
-
 const GroupHeader = styled('div')({
     position: 'sticky',
     top: '-1px',
@@ -133,35 +127,6 @@ const GroupHeader = styled('div')({
     '&.fairy': { backgroundColor: '#e48fe3' },
 });
 
-const RoundedButton = styled(ButtonBase)({
-    borderRadius: '3rem',
-    background: 'white',
-    fontSize: '.8rem',
-    color: '#000',
-    justifyContent: 'left',
-    boxShadow: '0 2px 2px 1px #88666666',
-    fontFamily: 'M PLUS 1p',
-    'svg': {
-        width: '1.2rem',
-        height: '1.2rem',
-        color: '#24da6d',
-    }
-});
-const SortOrderButton = styled(RoundedButton)({
-    padding: '.2rem',
-    translation: '0.2s',
-
-    'svg': {
-        width: '1rem', height: '1rem',
-        transform: 'translateY(0%)',
-        transition: '0.2s',
-    },
-    '&.desc svg': {
-        transform: 'rotate(180deg)',
-        transition: '0.2s',
-    },
-});
-
 /**
  * Pokemon select dialog configuration.
  */
@@ -187,9 +152,7 @@ const PokemonSelectDialog = React.memo(({
 }) => {
     const { t } = useTranslation();
     const [filterOpen, setFilterOpen] = React.useState(false);
-    const [sortMenuOpen, setSortMenuOpen] = React.useState(false);
     let [config_, setConfig] = React.useState<PokemonDialogConfig|null>(null);
-    const sortMenuAnchorRef = React.useRef<HTMLButtonElement>(null);
 
     // initialize config
     const config: PokemonDialogConfig = (config_ === null ?
@@ -201,9 +164,8 @@ const PokemonSelectDialog = React.memo(({
 
     // close handler
     const closeHandler = useCallback(() => {
-        setSortMenuOpen(false);
         onClose();
-    }, [setSortMenuOpen, onClose]);
+    }, [onClose]);
 
     // Compare inputed text to options
     const filterOptions = useCallback((
@@ -309,36 +271,20 @@ const PokemonSelectDialog = React.memo(({
     // Filter button
     const onFilterButtonClick = useCallback(() => {
         setFilterOpen(true);
-    }, [setFilterOpen]);
+    }, []);
     const onFilterDialogClose = useCallback(() => {
         setFilterOpen(false);
-    }, [setFilterOpen]);
+    }, []);
     const onFilterChange = useCallback((value: PokemonDialogConfig) => {
         setConfig(value);
         localStorage.setItem('PstPokemonSelectParam', JSON.stringify(value));
-    }, [setConfig]);
-
-    // Sort menu
-    const onSortButtonClick = useCallback((e: React.MouseEvent<HTMLElement>) => {
-        setSortMenuOpen(true);
-    }, [setSortMenuOpen]);
-    const onSortMenuClose = useCallback(() => {
-        setSortMenuOpen(false);
-    }, [setSortMenuOpen]);
-    const onConfigChange = React.useCallback((value: PokemonDialogConfig) => {
-        setConfig(value);
-        localStorage.setItem('PstPokemonSelectParam', JSON.stringify(value));
-    }, [setConfig]);
-    const onToggleSortOrder = useCallback(() => {
-        onConfigChange({...config, descending: !config.descending});
-    }, [config, onConfigChange]);
-    const onSortMenuItemSelected = useCallback((e: React.MouseEvent<HTMLLIElement>) => {
-        const value = e.currentTarget.getAttribute('data-value');
-        if (value !== null && value !== undefined) {
-            onConfigChange({...config, sort: value as "sleeptype"|"name"|"pokedexno"|"type"});
-            setSortMenuOpen(false);
-        }
-    }, [config, onConfigChange, setSortMenuOpen]);
+    }, []);
+    const onFilterConfigChange = useCallback((value: PokemonFilterConfig) => {
+        const newValue = {...config, descending: value.descending,
+            sort: value.sort as "sleeptype"|"name"|"pokedexno"|"type"};
+        setConfig(newValue);
+        localStorage.setItem('PstPokemonSelectParam', JSON.stringify(newValue));
+    }, [config]);
 
     // sort
     let options: PokemonOption[] = [];
@@ -408,35 +354,14 @@ const PokemonSelectDialog = React.memo(({
                 )}
             onClose={onAutocompleteClose}
             onChange={onAutocompleteChange}/>
-        <PokemonSelectDialogFooter>
-            <RoundedButton style={{padding: '.2rem .8rem', marginRight: '.5rem', width: "4.5rem", textAlign: 'left'}}
-                onClick={onFilterButtonClick}>
-                <SearchIcon style={{paddingRight: '0'}}/>{t(filterType === null && filterEvolve === "all" ? 'off' : 'on')}
-            </RoundedButton>
-            <RoundedButton style={{padding: '.2rem .8rem', marginRight: '.5rem', width: "8rem", textAlign: 'left'}}
-                onClick={onSortButtonClick} ref={sortMenuAnchorRef}>
-                <SortIcon style={{paddingRight: '.4rem'}}/>{t(sortType)}
-            </RoundedButton>
-            <Popper open={sortMenuOpen} anchorEl={sortMenuAnchorRef.current} style={{zIndex: 1500}}>
-                <Paper elevation={10}>
-                    <ClickAwayListener onClickAway={onSortMenuClose}>
-                        <MenuList>
-                            <MenuItem onClick={onSortMenuItemSelected} data-value="pokedexno"
-                                selected={sortType === 'pokedexno'}>{t('pokedexno')}</MenuItem>
-                            <MenuItem onClick={onSortMenuItemSelected} data-value="name"
-                                selected={sortType === 'name'}>{t('name')}</MenuItem>
-                            <MenuItem onClick={onSortMenuItemSelected} data-value="sleeptype"
-                                selected={sortType === 'sleeptype'}>{t('sleeptype')}</MenuItem>
-                            <MenuItem onClick={onSortMenuItemSelected} data-value="type"
-                                selected={sortType === 'type'}>{t('type')}</MenuItem>
-                        </MenuList>
-                    </ClickAwayListener>
-                </Paper>
-            </Popper>
-            <SortOrderButton className={descending ? 'desc' : 'asc'} onClick={onToggleSortOrder}>
-                <NorthIcon style={{width: '1rem', height: '1rem'}}/>
-            </SortOrderButton>
-        </PokemonSelectDialogFooter>
+        <PokemonFilterFooter
+            sortTypes={["pokedexno", "name", "sleeptype", "type"]}
+            value={{
+                isFiltered: config.filterEvolve !== "all" || config.filterType !== null,
+                sort: config.sort, descending: config.descending,
+            }}
+            onChange={onFilterConfigChange}
+            onFilterButtonClick={onFilterButtonClick}/>
         <PokemonFilterDialog open={filterOpen} onClose={onFilterDialogClose}
             value={config} onChange={onFilterChange}/>
     </StyledDialog>;
