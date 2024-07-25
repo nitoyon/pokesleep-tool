5f137f6a24c966fbc5f44dcafbd167dfcbf44034
nitoyon <nitoyon@gmail.com>
2024-07-25 23:09:36 +0900

    IV: Improve delete all UI

--- src/ui/IvCalc/BoxDeleteAllDialog.tsx
+++ src/ui/IvCalc/BoxDeleteAllDialog.tsx
@@ -14,25 +14,26 @@ const BoxDeleteAllDialog = React.memo(({ box, open, onClose }: {
     const onDelete = React.useCallback(() => {
         box.removeAll();
         setDeletedMessageVisible(true);
-    }, [setDeletedMessageVisible, box]);
+        onClose();
+    }, [box, onClose]);
     const onDeletedMessageClose = React.useCallback(() => {
         setDeletedMessageVisible(false);
-    }, [setDeletedMessageVisible]);
+    }, []);
 
-    return (
+    return (<>
         <Dialog open={open} onClose={onClose}>
             <DialogTitle>{t("delete all")}</DialogTitle>
             <DialogContent>
                 <p style={{ fontSize: "0.9rem", margin: 0 }}>{t("delete all message")}</p>
             </DialogContent>
             <DialogActions>
-                <Button onClick={onDelete}>{t("delete all pokemon")}</Button>
-                <Button onClick={onClose}>{t("close")}</Button>
+                <Button onClick={onDelete} color="error">{t("delete")}</Button>
+                <Button onClick={onClose}>{t("cancel")}</Button>
             </DialogActions>
-            <Snackbar open={deletedMessageVisible}
-                autoHideDuration={2000} onClose={onDeletedMessageClose}
-                message={t("deleted all pokemon")}/>
         </Dialog>
-    );
+        <Snackbar open={deletedMessageVisible}
+            autoHideDuration={2000} onClose={onDeletedMessageClose}
+            message={t("deleted all pokemon")}/>
+    </>);
 });
 export default BoxDeleteAllDialog;
