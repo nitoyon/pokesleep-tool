a5c36c338cb6419cdaa6ac7a16a88c733f5a6d73
nitoyon <nitoyon@gmail.com>
2024-08-03 13:37:39 +0900

    IV: Add carry limit to IvForm

---
+++ src/ui/IvCalc/CarryLimitTextField.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import { styled } from '@mui/system';
+import PokemonIv from '../../util/PokemonIv';
+import { MenuList, MenuItem, Popper, Paper, ClickAwayListener } from '@mui/material';
+import TextLikeButton from './TextLikeButton';
+import { useTranslation } from 'react-i18next';
+
+const CarryLimitTextField = React.memo(({iv, onChange}: {
+    iv: PokemonIv,
+    onChange: (evolvedCount: 0|1|2) => void,
+}) => {
+    const [open, setOpen] = React.useState(false);
+    const anchorRef = React.useRef<HTMLButtonElement>(null);
+  
+    const onClick = React.useCallback(() => {
+        setOpen((prevOpen) => !prevOpen);
+    }, [setOpen]);
+  
+    const onClose = React.useCallback(() => {
+        setOpen(false);
+    }, [setOpen]);
+  
+    // return focus to the button when we transitioned from !open -> open
+    const prevOpen = React.useRef(open);
+    React.useEffect(() => {
+        if (prevOpen.current === true && open === false) {
+            anchorRef.current!.focus();
+        }
+        prevOpen.current = open;
+    }, [open]);
+
+    if (iv.pokemon.evolutionCount <= 0) {
+        return <span style={{paddingBottom: '3px'}}>{iv.carryLimit}</span>;
+    }
+  
+    return <>
+            <TextLikeButton ref={anchorRef} onClick={onClick}
+                className={open ? "focused" : ""}
+                style={{width: '2.5rem'}}>
+                {iv.carryLimit}
+            </TextLikeButton>
+            <CarryLimitPopper open={open} anchorRef={anchorRef}
+                iv={iv} onClose={onClose} onChange={onChange}/>
+    </>;
+});
+
+const CarryLimitPopper = React.memo(({open, anchorRef, iv, onClose, onChange}: {
+    open: boolean,
+    anchorRef: React.RefObject<HTMLButtonElement>,
+    iv: PokemonIv,
+    onClose: () => void,
+    onChange: (evolvedCount: 0|1|2) => void,
+}) => {
+    const { t } = useTranslation();
+
+    const onClick = React.useCallback((event: React.MouseEvent<HTMLElement>) => {
+        const val = event.currentTarget.getAttribute('value');
+        if (val === null) { return; }
+        onClose();
+        onChange(parseInt(val) as 0|1|2);
+    }, [onChange, onClose]);
+
+    let menuItems: React.ReactNode[] = [];
+    const inventory = iv.subSkills.getActiveSubSkills(iv.level).reduce((p, c) => p + c.inventory, 0) * 6;
+    for (let i = 0; i <= iv.pokemon.evolutionCount; i++) {
+        const val = iv.pokemon.evolutionCount - i;
+        const carry = iv.pokemon.carryLimit + 5 * val + inventory;
+        let desc = "";
+        switch (val) {
+            case 0: desc = t('not evolved'); break;
+            case 1: desc = t('evolved once'); break;
+            case 2: desc = t('evolved twice'); break;
+        }
+        menuItems.push(<CarryLimitMenuItem key={val} value={val} dense
+            selected={val === iv.evolvedCount} onClick={onClick} >
+            {carry}
+            <span>({desc})</span>
+        </CarryLimitMenuItem>);
+    }
+
+    return <Popper open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        placement="bottom-start"
+        disablePortal
+        sx={{zIndex: 2}}>
+        <Paper elevation={12}>
+            <ClickAwayListener onClickAway={onClose}>
+                <MenuList>
+                    {menuItems}
+                </MenuList>
+            </ClickAwayListener>
+        </Paper>
+  </Popper>
+;
+});
+
+const CarryLimitMenuItem = styled(MenuItem)({
+    '& > span': {
+        paddingLeft: '0.8rem',
+        fontSize: '0.8rem',
+        color: '#999',
+    },
+});
+
+export default CarryLimitTextField;
