7bd7f4203b489f4235b0949577bf1e7dd49bf303
nitoyon <nitoyon@gmail.com>
2024-08-14 20:12:44 +0900

    IV: Update icon for Wobbuffet and Wynaut

--- src/ui/IvCalc/PokemonIconData.ts
+++ src/ui/IvCalc/PokemonIconData.ts
@@ -457,7 +457,9 @@ const pokemonRectData: {[id: string]: RectElement[]} = {
     ],
     // Wobbuffet
     '202': [
-        {x: 0, y: 0, w: 1, h: 1, color: '#8ce0f8'},
+        {x: 0.7, y: 0.8, w: 0.3, h: 0.25, r: 0.2, color: '#484848'},
+        {x: 0.2, y: 0.05, w: 0.6, h: 1.2, r: 0.2, color: '#8ce0f8'},
+        {x: 0.87, y: 0.87, w: 0.08, h: 0.08, r: 0.04, color: '#ffffff'}
     ],
     // Steelix
     '208': [
@@ -590,7 +592,11 @@ const pokemonRectData: {[id: string]: RectElement[]} = {
     ],
     // Wynaut
     '360': [
-        {x: 0, y: 0, w: 1, h: 1, color: '#96e9f0'},
+        {x: 0.15, y: 0.15, w: 0.7, h: 0.7, r: 0.33, color: '#96e9f0'},
+        {x: 0.6, y: 0.85, w: 0.35, h: 0.15, r: 0.1, color: '#494949'},
+        {x: 0.83, y: 0.89, w: 0.08, h: 0.08, r: 0.04, color: '#ffffff'},
+        {x: 0.26, y: 0.65, w: 0.48, h: 0.4, r: 0.3, color: '#96e9f0'},
+        {x: 0.25, y: 0.8, w: 0.5, h: 0.2, r: 0.1    , color: '#96e9f0'},
     ],
     // Spheal
     '363': [
