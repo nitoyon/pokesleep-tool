370ddc67734dddbc2b42b5339991133c582d25be
nitoyon <nitoyon@gmail.com>
2024-08-17 19:20:35 +0900

    IV: Update main skill icon

--- src/ui/IvCalc/MainSkillIcon.tsx
+++ src/ui/IvCalc/MainSkillIcon.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 import { MainSkillName } from '../../util/MainSkill';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
+import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
 import SwipeOutlinedIcon from '@mui/icons-material/SwipeOutlined';
 import SearchIcon from '@mui/icons-material/Search';
+import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
 import VolunteerActivismOutlinedIcon from '@mui/icons-material/VolunteerActivismOutlined';
 import DreamShardIcon from '../Resources/DreamShardIcon';
 import IngredientsIcon from '../Resources/IngredientsIcon';
 import PotIcon from '../Resources/PotIcon';
+import SavedSearchOutlined from '@mui/icons-material/SavedSearchOutlined';
 
 const MainSkillIcon = React.memo(({mainSkill}: {mainSkill: MainSkillName}) => {
     switch (mainSkill) {
         case "Charge Energy S":
-        case "Energizing Cheer S":
             return <svg width="20" height="20"><FavoriteBorderIcon sx={{color: "#ff88aa"}}/></svg>;
         case "Energy for Everyone S":
+            return <svg width="22" height="22"><VolunteerActivismIcon sx={{color: "#ff88aa"}}/></svg>;
+        case "Energizing Cheer S":
             return <svg width="22" height="22"><VolunteerActivismOutlinedIcon sx={{color: "#ff88aa"}}/></svg>;
         case "Charge Strength M":
         case "Charge Strength S":
@@ -24,13 +28,15 @@ const MainSkillIcon = React.memo(({mainSkill}: {mainSkill: MainSkillName}) => {
         case "Dream Shard Magnet S (Random)":
             return <svg width="20" height="20"><DreamShardIcon/></svg>;
         case "Extra Helpful S":
-        case "Helper Boost":
             return <svg width="20" height="20"><SearchIcon sx={{color: "#66cc66"}}/></svg>;
+        case "Helper Boost":
+            return <svg width="20" height="20"><SavedSearchOutlined sx={{color: "#66cc66"}}/></svg>;
         case "Ingredient Magnet S":
             return <svg width="20" height="20"><IngredientsIcon/></svg>;
         case "Cooking Power-Up S":
-        case "Tasty Chance S":
             return <svg width="20" height="20" fill="#886666"><PotIcon/></svg>;
+        case "Tasty Chance S":
+            return <svg width="20" height="20"><PriorityHighIcon sx={{color: "#ff0000"}}/></svg>;
         case "Metronome":
             return <svg width="22" height="22"><SwipeOutlinedIcon sx={{color: "#999"}}/></svg>;
         default:
