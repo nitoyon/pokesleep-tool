1d86041bb77b3f471afd59eb420e6cb6cadae8f6
nitoyon <nitoyon@gmail.com>
2024-09-08 23:37:32 +0900

    IV: Extract IngredientButton.tsx from BoxFilterDialog.tsx

--- src/ui/IvCalc/BoxFilterDialog.tsx
+++ src/ui/IvCalc/BoxFilterDialog.tsx
@@ -3,7 +3,7 @@ import { styled } from '@mui/system';
 import TypeButton from './TypeButton';
 import { BoxFilterConfig } from './BoxView';
 import { EditSubSkillControl } from './SubSkillControl';
-import IngredientIcon from './IngredientIcon';
+import IngredientButton from './IngredientButton';
 import MainSkillIcon from './MainSkillIcon';
 import { IngredientName, IngredientNames, PokemonType, PokemonTypes
 } from '../../data/pokemons';
@@ -145,41 +145,6 @@ const StyledTab = styled(Tab)({
     minWidth: '0',
     padding: '6px 8px',
     textTransform: 'none',
-});
-
-const IngredientButton = React.memo(({ingredient, checked, onClick}: {
-    ingredient: IngredientName,
-    checked: boolean,
-    onClick: (value: IngredientName) => void,
-}) => {
-    const onIngredientClick = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
-        const value = e.currentTarget.value as IngredientName;
-        onClick(value);
-    }, [onClick]);
-    return <StyledIngredientButton
-        key={ingredient} value={ingredient} onClick={onIngredientClick}>
-        <IngredientIcon name={ingredient}/>
-        {checked && <CheckIcon/>}
-    </StyledIngredientButton>;
-});
-
-const StyledIngredientButton = styled(Button)({
-    width: '20%',
-    height: '40px',
-    color: 'white',
-    fontSize: '0.9rem',
-    padding: 0,
-    margin: '0.2rem',
-    borderRadius: '0.5rem',
-    '& > svg.MuiSvgIcon-root': {
-        position: 'absolute',
-        background: '#24d76a',
-        borderRadius: '15px',
-        fontSize: '15px',
-        border: '2px solid white',
-        bottom: '0px',
-        right: '10px',
-    },
 });
 
 const MainSkillTab = React.memo(({value, onChange}: {
