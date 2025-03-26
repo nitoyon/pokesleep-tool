5311fd7190dc5a4a514543c9cbf7cd27cbf8fdf0
nitoyon <nitoyon@gmail.com>
2025-03-26 18:16:15 +0900

    IV: Fix Nature.test.ts

--- src/util/Nature.test.ts
+++ src/util/Nature.test.ts
@@ -8,7 +8,7 @@ describe('Nature', () => {
         expect(n.isSpeedOfHelpDown).toBe(true);
         expect(n.isExpGainsDown).toBe(false);
         expect(n.energyRecoveryFactor).toBe(1.2);
-        expect(n.speedOfHelpFactor).toBe(1.1);
+        expect(n.speedOfHelpFactor).toBe(1.075);
         expect(n.expGainsFactor).toBe(0);
         expect(n.upEffect).toBe("Energy recovery");
         expect(n.downEffect).toBe("Speed of help");
