976bdb70029669d2c688f9611e7dba2989ea35c3
nitoyon <nitoyon@gmail.com>
2025-04-06 20:04:27 +0900

    IV: Extract IngredientCountIcon from IngredientTextField

---
+++ src/ui/IvCalc/IngredientCountIcon.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { styled } from '@mui/system';
+import { IngredientName } from '../../data/pokemons';
+import { Badge } from '@mui/material';
+import IngredientIcon from './IngredientIcon';
+
+const IngredientBadge = styled(Badge)(({ theme }) => ({
+    marginRight: '.8rem',
+    width: 26,
+    height: 26,
+    '& .MuiBadge-badge': {
+        top: 23,
+        right: 4,
+        border: '1px solid #ccbb88',
+        fontSize: '0.5rem',
+        height: '0.7rem',
+        width: '0.8rem',
+        color: "#000",
+        backgroundColor: "#ffffff",
+        boxShadow: '0 1px 2px 1px rgba(128, 128, 128, 0.4)',
+        zIndex: 0,
+    },
+    '& > svg': {
+        width: '20px',
+        height: '20px',
+    },
+}));
+
+const IngredientCountIcon = React.memo(({name, count}: {
+    name: IngredientName,
+    count: number,
+}) => {
+    return <IngredientBadge badgeContent={"×"+count}>
+        <IngredientIcon name={name}/>
+    </IngredientBadge>;
+});
+
+export default IngredientCountIcon;
--- src/ui/IvCalc/IngredientTextField.tsx
+++ src/ui/IvCalc/IngredientTextField.tsx
@@ -2,8 +2,8 @@ import React, { useCallback } from 'react';
 import { styled } from '@mui/system';
 import { IngredientType } from '../../util/PokemonRp';
 import { PokemonData } from '../../data/pokemons';
-import { Badge, TextField, MenuItem } from '@mui/material';
-import IngredientIcon from './IngredientIcon';
+import { TextField, MenuItem } from '@mui/material';
+import IngredientCountIcon from './IngredientCountIcon';
 
 const BorderedMenuItem = styled(MenuItem)({
     borderTop: '1px dashed #ddd',
@@ -48,48 +48,23 @@ const IngredientTextField = React.memo(({pokemon, value, onChange}: {
     );
 });
 
-const IngredientBadge = styled(Badge)(({ theme }) => ({
-    marginRight: '.8rem',
-    width: 26,
-    height: 26,
-    '& .MuiBadge-badge': {
-        top: 23,
-        right: 4,
-        border: '1px solid #ccbb88',
-        fontSize: '0.5rem',
-        height: '0.7rem',
-        width: '0.8rem',
-        color: "#000",
-        backgroundColor: "#ffffff",
-        boxShadow: '0 1px 2px 1px rgba(128, 128, 128, 0.4)',
-        zIndex: 0,
-    },
-    '& > svg': {
-        width: '20px',
-        height: '20px',
-    },
-}));
-
 const PokemonIngredient = React.memo(({pokemon, value}: {
     pokemon: PokemonData,
     value: IngredientType,
 }) => {
-    const icon1 = <IngredientBadge badgeContent={"×"+pokemon.ing1.c1}>
-        <IngredientIcon name={pokemon.ing1.name}/>
-    </IngredientBadge>;
+    const icon1 = <IngredientCountIcon count={pokemon.ing1.c1}
+        name={pokemon.ing1.name}/>
 
     const ing2char = value.charAt(1);
     const ing2 = ing2char === 'A' ? pokemon.ing1 : pokemon.ing2;
-    const icon2 = <IngredientBadge badgeContent={"×"+ing2.c2}>
-        <IngredientIcon name={ing2.name}/>
-    </IngredientBadge>;
+    const icon2 = <IngredientCountIcon count={ing2.c2}
+        name={ing2.name}/>
     
     const ing3char = value.charAt(2);
     const ing3 = ing3char === 'C' && pokemon.ing3 !== undefined ? pokemon.ing3 :
         ing3char === 'A' ? pokemon.ing1 : pokemon.ing2;
-    const icon3 = <IngredientBadge badgeContent={"×"+ing3.c3}>
-        <IngredientIcon name={ing3.name}/>
-    </IngredientBadge>;
+    const icon3 = <IngredientCountIcon count={ing3.c3}
+        name={ing3.name}/>
     return <>{icon1}{icon2}{icon3}</>;
 });
 
