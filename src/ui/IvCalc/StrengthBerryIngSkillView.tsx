de30fce53fc39d84c8c52d69a737dca81fd0ab56
nitoyon <nitoyon@gmail.com>
2025-07-09 10:52:28 +0900

    IV: Improve first info button on strength tab

--- src/ui/IvCalc/StrengthBerryIngSkillView.tsx
+++ src/ui/IvCalc/StrengthBerryIngSkillView.tsx
@@ -13,7 +13,7 @@ import { ingredientStrength } from '../../util/PokemonRp';
 import { getSkillRandomRange as getSkillRange, getMaxSkillLevel, getSkillValue,
     MainSkillName } from '../../util/MainSkill';
 import { AmountOfSleep } from '../../util/TimeUtil';
-import { Button, Dialog, DialogActions, DialogTitle, DialogContent,
+import { Button, Dialog, DialogActions, DialogContent,
     FormControl, Select, SelectChangeEvent, ToggleButtonGroup, ToggleButton,
     Typography, MenuItem } from '@mui/material';
 import MainSkillIcon from './MainSkillIcon';
@@ -24,6 +24,7 @@ import { IvAction } from './IvState';
 import EnergyDialog from './EnergyDialog';
 import IngredientIcon from './IngredientIcon';
 import IngredientCountIcon from './IngredientCountIcon';
+import SpecialtyButton from './SpecialtyButton';
 import { useTranslation, Trans } from 'react-i18next';
 import i18next from 'i18next'
 
@@ -234,7 +235,7 @@ const StrengthBerryIngSkillStrengthView = React.memo(({
             <LocalFireDepartmentIcon sx={{color: "#ff944b"}}/>
             <span className="strength">{formatWithComma(Math.round(result.totalStrength))}</span>
             <InfoButton onClick={onHelpClick}/>
-            <HelpDialog open={helpOpen} onClose={onHelpClose}/>
+            <HelpDialog result={result} open={helpOpen} onClose={onHelpClose}/>
             {decendants.length === 1 && <span className="evolved">
                 {t('strength of x', {x: t(`pokemons.${pokemonIv.pokemon.name}`)})}
             </span>}
@@ -371,25 +372,68 @@ function getMainSkillArticle(pokemonIv: PokemonIv, result: StrengthResult,
     </article>;
 }
 
-const HelpDialog = React.memo(({open, onClose}: {
+const HelpDialog = React.memo(({result, open, onClose}: {
+    result: StrengthResult,
     open: boolean,
     onClose: () => void,
 }) => {
     const { t } = useTranslation();
 
-    return <Dialog open={open} onClose={onClose}>
-        <DialogTitle>{t('strength2')}</DialogTitle>
-        <DialogContent dividers style={{fontSize: '0.95rem'}}>
+    return <StyledHelpDialog open={open} onClose={onClose}>
+        <article>
+            <h1>
+                <LocalFireDepartmentIcon sx={{color: "#ff944b"}}/>
+                <span style={{transform: 'scale(1, 0.9)'}}>{formatWithComma(Math.round(result.totalStrength))}</span>
+            </h1>
+            <div className="grid">
+                <SpecialtyButton disabled specialty="Berries"/>
+                <div>{formatWithComma(Math.round(result.berryTotalStrength))}</div>
+                <SpecialtyButton disabled specialty="Ingredients"/>
+                <div>{formatWithComma(Math.round(result.ingStrength))}</div>
+                <SpecialtyButton disabled specialty="Skills"/>
+                <div>{formatWithComma(Math.round(result.skillStrength))}</div>
+            </div>
             <p style={{marginTop: 0}}>{t('strength detail1')}</p>
             <p>{t('strength detail2')}</p>
             <ul style={{paddingLeft: '1rem'}}>
                 <li>{t('strength restriction2')}</li>
             </ul>
-        </DialogContent>
+        </article>
         <DialogActions>
             <Button onClick={onClose}>{t('close')}</Button>
         </DialogActions>
-    </Dialog>;
+    </StyledHelpDialog>;
+});
+
+const StyledHelpDialog = styled(Dialog)({
+    '& article': {
+        margin: '0.8rem 0.8rem 0 0.8rem',
+        '& > h1': {
+            fontSize: '1.4rem',
+            margin: 0,
+            display: 'flex',
+            alignItems: 'center',
+        },
+        '& > div.grid': {
+            margin: '0.2rem 0 1.5rem 1rem',
+            display: 'grid',
+            gridTemplateColumns: 'min-content min-content',
+            gridGap: '0.2rem 0.5rem',
+            alignItems: 'center',
+            '& > div': {
+                textAlign: 'right',
+                fontWeight: 'bold',
+            },
+        },
+        '& > p': {
+            margin: '0.5rem 0',
+            fontSize: '0.9rem',
+        },
+        '& > ul': {
+            margin: 0,
+            fontSize: '0.9rem',
+        }
+    },
 });
 
 const BerryHelpDialog = React.memo(({open, onClose, strength, result}: {
