590162ed09184ff0977f47b41bd063ffdc006ae4
nitoyon <nitoyon@gmail.com>
2025-07-14 23:58:53 +0900

    IV: Extract Berry, Ing, Skill help dialog from StrengthBerryIngSkillView

---
+++ src/ui/IvCalc/Strength/IngHelpDialog.tsx
@@ -0,0 +1,251 @@
+import React from 'react';
+import { getEventBonus } from '../../../data/events';
+import { round1, round2, formatNice, formatWithComma } from '../../../util/NumberUtil';
+import PokemonStrength, { IngredientStrength, StrengthResult,
+    recipeLevelBonus
+} from '../../../util/PokemonStrength';
+import { StrengthParameter } from '../../../util/PokemonStrength';
+import { ingredientStrength } from '../../../util/PokemonRp';
+import { Button, Dialog, DialogActions, DialogContent,
+    FormControl, Select, SelectChangeEvent, Typography,
+    MenuItem } from '@mui/material';
+import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
+import InfoButton from '../InfoButton';
+import { IvAction } from '../IvState';
+import IngredientIcon from '../IngredientIcon';
+import IngredientCountIcon from '../IngredientCountIcon';
+import { StyledInfoDialog } from './StrengthBerryIngSkillView';
+import { useTranslation, Trans } from 'react-i18next';
+import i18next from 'i18next'
+
+const IngHelpDialog = React.memo(({open, strength, result, dispatch, onClose}: {
+    open: boolean,
+    strength: PokemonStrength,
+    result: StrengthResult,
+    dispatch: React.Dispatch<IvAction>,
+    onClose: () => void,
+}) => {
+    const { t } = useTranslation();
+    const [recipeBonusHelpOpen, setRecipeBonusHelpOpen] = React.useState(false);
+    const onChange = React.useCallback((value: StrengthParameter) => {
+        dispatch({type: "changeParameter", payload: {parameter: value}});
+    }, [dispatch]);
+    const onRecipeBonusChange = React.useCallback((e: SelectChangeEvent) => {
+        onChange({...strength.parameter, recipeBonus: parseInt(e.target.value)});
+    }, [onChange, strength.parameter]);
+    const onRecipeLevelChange = React.useCallback((e: SelectChangeEvent) => {
+        onChange({...strength.parameter, recipeLevel: parseInt(e.target.value) as 1|10|20|30|40|50|55});
+    }, [onChange, strength.parameter]);
+    const onRecipeBonusInfoClick = React.useCallback(() => {
+        setRecipeBonusHelpOpen(true);
+    }, []);
+    const onRecipeBonusHelpClose = React.useCallback(() => {
+        setRecipeBonusHelpOpen(false);
+    }, []);
+
+    if (!open) {
+        return <></>;
+    }
+
+    const param = strength.parameter;
+    console.log(param);
+    if (param.tapFrequency === 'none') {
+        return (
+            <Dialog open={open} onClose={onClose}>
+                <DialogContent style={{fontSize: '0.95rem', whiteSpace: 'pre-wrap'}}>
+                    {t('no ingredient')}
+                </DialogContent>
+
+                <DialogActions>
+                    <Button onClick={onClose}>{t('close')}</Button>
+                </DialogActions>
+            </Dialog>
+        );
+    }
+
+    const ingSlot = strength.pokemonIv.level < 30 ? 1 :
+        strength.pokemonIv.level < 60 ? 2 : 3;
+    const ings = [[result.ing1]];
+    if (ingSlot > 1) {
+        if (result.ing2.name === "unknown") {
+            // Darkrai not unlocked
+        }
+        else if (ings[0][0].name === result.ing2.name) {
+            ings[0].push(result.ing2);
+        }
+        else {
+            ings.push([result.ing2]);
+        }
+    }
+    if (ingSlot > 2 && result.ing3 !== undefined) {
+        if (result.ing3.name === "unknown") {
+            // Darkrai not unlocked
+        }
+        else if (ings[0][0].name === result.ing3.name) {
+            ings[0].push(result.ing3);
+        }
+        else if (ings.length > 1 && ings[1][0].name === result.ing3.name) {
+            ings[1].push(result.ing3);
+        }
+        else {
+            ings.push([result.ing3]);
+        }
+    }
+
+    const ingInRecipeStrengthRatio = param.recipeBonus === 0 ? 1 :
+        (1 + param.recipeBonus / 100) * (1 + recipeLevelBonus[param.recipeLevel] / 100);
+    const recipeRatio = ingInRecipeStrengthRatio * 0.8 + 0.2;
+    const dishBonus = getEventBonus(param.event, param.customEventBonus)?.dish ?? 1;
+
+    return <StyledInfoDialog open={open} onClose={onClose}>
+        <header>
+            <h1>
+                <LocalFireDepartmentIcon sx={{color: "#ff944b"}}/>
+                {formatWithComma(Math.round(result.ingStrength))}
+            </h1>
+            <div className={`grid ings${ings.length}`}>
+                {getIngDetail(strength, result, recipeRatio, ingSlot, ings[0], t)}
+                {ings.length > 1 && getIngDetail(strength, result, recipeRatio, ingSlot, ings[1], t)}
+                {ings.length > 2 && getIngDetail(strength, result, recipeRatio, ingSlot, ings[2], t)}
+            </div>
+        </header>
+        <article>
+            <div><span className="box box3">{round2(result.ingHelpCount)}</span></div>
+            <span>{t('ing help count')}</span>
+            <footer>
+                {round1(result.notFullHelpCount)}
+                <small> ({t('normal help count')})</small>
+                <> × </>
+                {round1(result.ingRatio * 100)}%
+                <small> ({t('ingredient rate')})</small>
+            </footer>
+            <div><span className="box box1">{round1(ings[0].reduce((p, c) => p + c.count, 0))}</span></div>
+            <span>{t('ing count')}</span>
+            <div><span className="box box2">{ingredientStrength[ings[0][0].name]}</span></div>
+            <span>{t('strength per ing')}</span>
+            <div><span className="box box5">{round2(recipeRatio)}</span></div>
+            <span>{t('recipe multiplier')}<InfoButton onClick={onRecipeBonusInfoClick}/></span>
+            <footer>
+                <>(</>
+                {round2(param.recipeBonus === 0 ? 1 : 1 + param.recipeBonus / 100)}
+                <small> ({t('recipe bonus')})</small>
+                <> × </>
+                {round2(1 + recipeLevelBonus[param.recipeLevel] / 100)}
+                <small>({t('average recipe level')})</small>
+                <>) × 0.8 + 0.2</>
+            </footer>
+            <div><span className="box box4">{param.fieldBonus}%</span></div>
+            <span>{t('area bonus')}</span>
+            {dishBonus !== 1 && <>
+                <div><span className="box box6">{dishBonus}</span></div>
+                <span>{t('event bonus')}</span>
+            </>}
+        </article>
+        <section style={{marginTop: '1.8rem'}}>
+            <label>{t('recipe bonus')}:</label>
+            <FormControl size="small">
+            <Select variant="standard" value={strength.parameter.recipeBonus.toString()}
+                onChange={onRecipeBonusChange}>
+                <MenuItem value={0}>0% <small style={{paddingLeft: '0.3rem'}}>({t('mixed recipe')})</small></MenuItem>
+                <MenuItem value={19}>19% <small style={{paddingLeft: '0.3rem'}}>(7{t('range separator')}16 {t('ingredients unit')})</small></MenuItem>
+                <MenuItem value={20}>20% <small style={{paddingLeft: '0.3rem'}}>(20{t('range separator')}22 {t('ingredients unit')})</small></MenuItem>
+                <MenuItem value={21}>21% <small style={{paddingLeft: '0.3rem'}}>(23{t('range separator')}26 {t('ingredients unit')})</small></MenuItem>
+                <MenuItem value={25}>25% <small style={{paddingLeft: '0.3rem'}}>(17{t('range separator')}35 {t('ingredients unit')})</small></MenuItem>
+                <MenuItem value={35}>35% <small style={{paddingLeft: '0.3rem'}}>(35{t('range separator')}56 {t('ingredients unit')})</small></MenuItem>
+                <MenuItem value={48}>48% <small style={{paddingLeft: '0.3rem'}}>(49{t('range separator')}77 {t('ingredients unit')})</small></MenuItem>
+                <MenuItem value={61}>61% <small style={{paddingLeft: '0.3rem'}}>(87{t('range separator')}102 {t('ingredients unit')})</small></MenuItem>
+            </Select></FormControl>
+        </section>
+        <section>
+            <label>{t('average recipe level')}:</label>
+            <Select variant="standard" value={strength.parameter.recipeLevel.toString()}
+                onChange={onRecipeLevelChange}>
+                <MenuItem value={1}>1</MenuItem>
+                <MenuItem value={10}>10</MenuItem>
+                <MenuItem value={20}>20</MenuItem>
+                <MenuItem value={30}>30</MenuItem>
+                <MenuItem value={40}>40</MenuItem>
+                <MenuItem value={50}>50</MenuItem>
+                <MenuItem value={55}>55</MenuItem>
+                <MenuItem value={60}>60</MenuItem>
+            </Select>
+        </section>
+        <DialogActions>
+            <Button onClick={onClose}>{t('close')}</Button>
+        </DialogActions>
+        <RecipeBonusHelpDialog open={recipeBonusHelpOpen} onClose={onRecipeBonusHelpClose}/>
+    </StyledInfoDialog>;
+});
+
+function getIngDetail(strength: PokemonStrength, result: StrengthResult,
+    recipeRatio: number, ingSlot: number,
+    ing: IngredientStrength[], t: typeof i18next.t): React.ReactNode {
+    if (strength.parameter.tapFrequency === 'none') {
+        return <article>ー</article>;
+    }
+
+    const count = ing.reduce((p, c) => p + c.count, 0);
+    const ingName = ing[0].name;
+    const param = strength.parameter;
+    const dishBonus = getEventBonus(param.event, param.customEventBonus)?.dish ?? 1;
+
+    return <>
+        <span>
+            <IngredientIcon name={ing[0].name}/>
+            {round1(count)}
+        </span>
+        <div>
+            <span className="box box3">{round2(result.ingHelpCount)}</span>
+            <> × </>
+            {ing.length > 1 ? '(' : ''}
+            {ing.map((ing, i) => <span key={i}>
+                {i === 0 ? '' : ' + '}
+                <IngredientCountIcon count={ing.helpCount} name={ingName}/>
+            </span>)}
+            {ing.length > 1 ? ')' : ''}
+            <>{ingSlot > 1 ? ` / ${ingSlot}` : ''}</>
+        </div>
+        <span style={{marginTop: '-0.5rem'}}>
+            <LocalFireDepartmentIcon sx={{color: "#ff944b"}} className="strength"/>
+            {formatNice(ing.reduce((p, c) => p + c.strength, 0), t)}
+        </span>
+        <div style={{marginTop: '-0.5rem'}}>
+            <span className="box box1">{round1(count)}</span><> × </>
+            <span className="box box2">{ingredientStrength[ingName]}</span><> × </>
+            <span className="box box5">{round2(recipeRatio)}</span><> × </>
+            <span>
+                <>(1+</>
+                <span className="box box4">{param.fieldBonus}%</span>
+                <>)</>
+            </span>
+            {dishBonus !== 1 && <>
+                <> × </>
+                <span className="box box6">{dishBonus}</span>
+            </>}
+        </div>
+    </>;
+}
+
+const RecipeBonusHelpDialog = React.memo(({open, onClose}: {
+    open: boolean,
+    onClose: () => void,
+}) => {
+    const { t } = useTranslation();
+
+    return <Dialog open={open} onClose={onClose}>
+        <DialogContent>
+            <Typography paragraph>
+                <Trans i18nKey="recipe bonus help"
+                    components={{
+                        raenonx: <a href={t('recipe bonus list')}>raenonx</a>,
+                    }}/>
+            </Typography>
+            <Typography variant="body2">{t('recipe strength help')}</Typography>
+        </DialogContent>
+        <DialogActions>
+            <Button onClick={onClose}>{t('close')}</Button>
+        </DialogActions>
+    </Dialog>;
+});
+
+export default IngHelpDialog;
---
+++ src/ui/IvCalc/Strength/SkillHelpDialog.tsx
@@ -0,0 +1,336 @@
+import React from 'react';
+import { getEventBonus, getEventBonusIfTarget } from '../../../data/events';
+import Nature from '../../../util/Nature';
+import { round1, round2, formatNice, formatWithComma } from '../../../util/NumberUtil';
+import PokemonStrength, { StrengthResult } from '../../../util/PokemonStrength';
+import { getSkillRandomRange as getSkillRange, getMaxSkillLevel, getSkillValue,
+    MainSkillName } from '../../../util/MainSkill';
+import { Button, Dialog, DialogActions, DialogContent,
+    FormControl, ToggleButtonGroup, ToggleButton } from '@mui/material';
+import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
+import MainSkillIcon from '../MainSkillIcon';
+import { StyledNatureUpEffect, StyledNatureDownEffect } from '../IvForm/NatureTextField';
+import { IvAction } from '../IvState';
+import { StyledInfoDialog } from './StrengthBerryIngSkillView';
+import { useTranslation, Trans } from 'react-i18next';
+import i18next from 'i18next'
+
+const SkillHelpDialog = React.memo(({open, dispatch, onClose, strength, result}: {
+    open: boolean,
+    dispatch: React.Dispatch<IvAction>,
+    onClose: () => void,
+    strength: PokemonStrength,
+    result: StrengthResult,
+}) => {
+    const { t } = useTranslation();
+
+    const onSkillLevelChange = React.useCallback((e: any, value: string|null) => {
+        if (value === null) {
+            return;
+        }
+        const iv = strength.pokemonIv.clone();
+        iv.skillLevel = parseInt(e.target.value);
+        dispatch({type: "updateIv", payload: {iv}});
+    }, [dispatch, strength.pokemonIv]);
+
+    const settings = strength.parameter;
+    if (settings.period === 3 || settings.tapFrequency === 'none') {
+        return (
+            <Dialog open={open} onClose={onClose}>
+                <DialogContent style={{fontSize: '0.95rem', whiteSpace: 'pre-wrap'}}>
+                    {t('strength skill info.not triggered')}
+                </DialogContent>
+
+                <DialogActions>
+                    <Button onClick={onClose}>{t('close')}</Button>
+                </DialogActions>
+            </Dialog>
+        );
+    }
+
+    const iv = strength.pokemonIv;
+    const skill = iv.pokemon.skill.replace(" (Random)", "");
+    const skillValue = result.skillCount === 0 ? 0 :
+        Math.round(result.skillValue / result.skillCount);
+    const footnote = t(`strength skill info.${skill}`);
+    const skillName = iv.pokemon.skill;
+    const isCountOnly = skillName === "Metronome" ||
+        skillName.startsWith("Skill Copy");
+
+    const maxSkillLevel = getMaxSkillLevel(iv.pokemon.skill);
+    const toggleButtons = Array.from({ length: maxSkillLevel }, (_, i) => (
+        <ToggleButton key={i + 1} value={i + 1}>
+            {i + 1}
+        </ToggleButton>
+    ));
+
+    const skillLevel = strength.getSkillLevel();
+    const eventBonus = getEventBonusIfTarget(settings.event,
+        settings.customEventBonus, iv.pokemon);
+    const [skillValueText, skillValueFooter] = getSkillValueText(strength,
+        skillLevel, t);
+
+    return <StyledInfoDialog open={open} onClose={onClose}>
+        <header>
+            <h1>
+                <MainSkillIcon mainSkill={skillName}/>
+                {formatNice(result.skillValue, t)}
+            </h1>
+            {!isCountOnly && <h2>
+                <span className="box box1">{formatWithComma(skillValue)}</span><> × </>
+                <span className="box box2">{round2(result.skillCount)}</span>
+            </h2>}
+            {skill === "Energy for Everyone S (Lunar Blessing)" && <h1>
+                <LocalFireDepartmentIcon sx={{color: "#ff944b"}}/>
+                {formatNice(result.skillStrength, t)}
+            </h1>}
+        </header>
+        <article>
+            {!isCountOnly && <>
+                <div><span className="box box1">{formatWithComma(skillValue)}</span></div>
+                <span>{skillValueText}</span>
+                {skillValueFooter !== null && <footer>{skillValueFooter}</footer>}
+            </>}
+            <div><span className="box box2">{round2(result.skillCount)}</span></div>
+            <span>{t('skill count')}</span>
+            <footer>
+                {round1(result.notFullHelpCount)}
+                <small> ({t('normal help count')})</small>
+                <> × </>
+                {round1(result.skillRatio * 100)}%
+                <small> ({t('skill rate')})</small>
+            </footer>
+        </article>
+        {!isCountOnly &&
+            <section style={{marginTop: '1.8rem'}}>
+                <label>{t('skill level')}:</label>
+                <FormControl size="small">
+                    <ToggleButtonGroup exclusive
+                        value={iv.skillLevel} onChange={onSkillLevelChange}>
+                        {toggleButtons}
+                    </ToggleButtonGroup>
+                </FormControl>
+            </section>
+        }
+        {!isCountOnly && ((eventBonus?.skillLevel ?? 0) > 0 || settings.maxSkillLevel) &&
+        skillLevel !== iv.skillLevel &&
+                <div className="skillLevelNotice">
+                    <Trans i18nKey={settings.maxSkillLevel ? "max skill level affected" : "skill level bonus affected"}
+                        components={{ level: <strong>{skillLevel}</strong>}}/>
+                </div>
+        }
+        {footnote !== "" && <div className="footnote">{footnote}</div>}
+        <DialogActions>
+            <Button onClick={onClose}>{t('close')}</Button>
+        </DialogActions>
+    </StyledInfoDialog>;
+});
+
+function getSkillValueText(strength: PokemonStrength, skillLevel: number,
+    t: typeof i18next.t):
+[React.ReactNode, React.ReactNode]{
+    const skill: MainSkillName = strength.pokemonIv.pokemon.skill;
+
+    if (skill.startsWith('Charge Energy')) {
+        return getChargeEnergyValueText(strength, skillLevel, t);
+    }
+    if (skill.startsWith('Charge Strength')) {
+        return getChargeStrengthValueText(strength, skillLevel, t);
+    }
+    if (skill.startsWith('Ingredient Magnet S') ||
+        skill.startsWith("Ingredient Draw S")
+    ) {
+        return getIngredientGetValueText(strength, skillLevel, t);
+    }
+    if (skill.startsWith('Dream Shard Magnet')) {
+        return getDreamShardMagnetValueText(strength, skillLevel, t);
+    }
+    if (skill === 'Energizing Cheer S') {
+        return getEnergyRecoveryValueText(strength, skillLevel, t,
+            t('nature effect.Energy recovery'));
+    }
+    if (skill.startsWith('Energy for Everyone S')) {
+        return getEnergyRecoveryValueText(strength, skillLevel, t,
+            t('e4e per pokemon'));
+    }
+    if (skill.startsWith('Cooking Power-Up S')) {
+        return getNormalSkillValueText(t, t('pot size power up'));
+    }
+    if (skill === 'Tasty Chance S') {
+        return getNormalSkillValueText(t, t('tasty chance increase'));
+    }
+    if (skill === 'Extra Helpful S') {
+        return getNormalSkillValueText(t, t('help count'));
+    }
+    if (skill === 'Helper Boost') {
+        return getNormalSkillValueText(t, t('help count per pokemon'));
+    }
+    if (skill.startsWith('Berry Burst')) {
+        return getNormalSkillValueText(t, t('berry strength per berry burst'));
+    }
+    return [null, null];
+}
+
+function getChargeEnergyValueText(strength: PokemonStrength,
+    skillLevel: number, t: typeof i18next.t):
+[React.ReactNode, React.ReactNode] {
+    const iv = strength.pokemonIv;
+    const skill: MainSkillName = strength.pokemonIv.pokemon.skill;
+
+    const text = t('value per skill', { value: t('nature effect.Energy recovery')});
+    const val = getSkillValue(skill, skillLevel);
+    return [<>
+        {text}<br/>
+        <span className="box box4">{val}</span>
+        <> × </>
+        <span className="box box5">{iv.nature.energyRecoveryFactor}</span>
+        <ul className="detail">
+            <li>
+                <span className="box box4">{val}</span>
+                <>: {t('base value', { value: t('nature effect.Energy recovery')})}</>
+            </li>
+            <li>
+                <span className="box box5">{iv.nature.energyRecoveryFactor}</span>
+                <>: {t('nature factor')}</>
+                {iv.nature.energyRecoveryFactor > 1 &&
+                <small> (<StyledNatureUpEffect>{t('nature effect.Energy recovery')}</StyledNatureUpEffect>)</small>}
+                {iv.nature.energyRecoveryFactor < 1 &&
+                <small> (<StyledNatureDownEffect>{t('nature effect.Energy recovery')}</StyledNatureDownEffect>)</small>}
+            </li>
+        </ul>
+    </>, null];
+}
+
+function getChargeStrengthValueText(strength: PokemonStrength,
+    skillLevel: number, t: typeof i18next.t):
+[React.ReactNode, React.ReactNode] {
+    const param = strength.parameter;
+    const skill: MainSkillName = strength.pokemonIv.pokemon.skill;
+
+    const text = t('value per skill', { value: t('strength2')});
+    let detail: React.ReactNode = null;
+    if (skill === 'Charge Strength S (Random)') {
+        const [min, max] = getSkillRange(skill, skillLevel);
+        detail = <small> ({t('range average', { min: formatWithComma(min), max: formatWithComma(max)})})</small>;
+    }
+    else if (skill === 'Charge Strength S (Stockpile)') {
+        const [min, max] = getSkillRange(skill, skillLevel);
+        detail = <small> ({t('range expected', { min: formatWithComma(min), max: formatWithComma(max) })})</small>;
+    }
+    const val = getSkillValue(skill, skillLevel);
+    return [<>
+        {text}<br/>
+        <span className="box box4">{val}</span>
+        <> × (1 + </>
+        <span className="box box5">{param.fieldBonus}%</span>
+        <>)</>
+        <ul className="detail">
+            <li>
+                <span className="box box4">{val}</span>
+                <>: {t('base value', { value: t('strength2')})}</>
+                {detail}
+            </li>
+            <li>
+                <span className="box box5">{param.fieldBonus}%</span>
+                <>: {t('area bonus')}</>
+            </li>
+        </ul>
+    </>, null];
+}
+
+function getIngredientGetValueText(strength: PokemonStrength,
+    skillLevel: number, t: typeof i18next.t):
+[React.ReactNode, React.ReactNode] {
+    const param = strength.parameter;
+    const val = getSkillValue(strength.pokemonIv.pokemon.skill, skillLevel);
+    const ingBonus = getEventBonus(param.event, param.customEventBonus)?.ingredientMagnet ?? 1;
+
+    const text = t('value per skill', { value: t('ing count')});
+    if (ingBonus === 1) {
+        return [text, null];
+    }
+
+    return [text, <>
+        {val}
+        <small> ({t('base value', { value: t('ing count')})})</small>
+        <> × </>
+        {ingBonus}
+        <small> ({t('event bonus')})</small>
+    </>];
+}
+
+function getDreamShardMagnetValueText(strength: PokemonStrength,
+    skillLevel: number, t: typeof i18next.t):
+[React.ReactNode, React.ReactNode] {
+    const param = strength.parameter;
+    const skill: MainSkillName = strength.pokemonIv.pokemon.skill;
+
+    const shardBonus = getEventBonus(param.event, param.customEventBonus)?.dreamShard ?? 1;
+    const text = t('value per skill', { value: t('dream shard')});
+    if (skill === 'Dream Shard Magnet S') {
+        if (shardBonus === 1) {
+            return [text, null];
+        }
+        else {
+            const val = getSkillValue(skill, skillLevel);
+            return [text, <>
+                <>
+                    {val}
+                    <small> ({t('base value', { value: t('dream shard')})})</small>
+                    <> × </>
+                    {shardBonus}
+                    <small> ({t('event bonus')})</small>
+                </>
+            </>];
+        }
+    }
+    else if (skill === 'Dream Shard Magnet S (Random)') {
+        const [min, max] = getSkillRange(skill, skillLevel);
+        if (shardBonus === 1) {
+            return [text, <>{t('range average', { min, max})}</>];
+        }
+        else {
+            const val = getSkillValue(skill, skillLevel);
+            return [text,
+                <>
+                    {val}
+                    <small> ({t('range average', { min, max})})</small>
+                    <> × </>
+                    {shardBonus}
+                    <small> ({t('event bonus')})</small>
+                </>];
+        }
+    }
+
+    return [null, null];
+}
+
+function getEnergyRecoveryValueText(strength: PokemonStrength,
+    skillLevel: number, t: typeof i18next.t, valueText: string
+):
+[React.ReactNode, React.ReactNode] {
+    const text = t('value per skill', { value: valueText});
+    const val = getSkillValue(strength.pokemonIv.pokemon.skill, skillLevel);
+    return [<>
+        {text}<br/>
+        <ul className="detail">
+            <li>
+                <StyledNatureUpEffect>{t('nature effect.Energy recovery')}</StyledNatureUpEffect>
+                <>: </>
+                {Math.floor(val * new Nature("Bold").energyRecoveryFactor)}
+            </li>
+            <li>
+                <StyledNatureDownEffect>{t('nature effect.Energy recovery')}</StyledNatureDownEffect>
+                <>: </>
+                {Math.floor(val * new Nature("Hasty").energyRecoveryFactor)}
+            </li>
+        </ul>
+    </>, null];
+}
+
+function getNormalSkillValueText(t: typeof i18next.t, valueText: string):
+[React.ReactNode, React.ReactNode] {
+    return [t('value per skill', { value: valueText}), null];
+}
+
+export default SkillHelpDialog;
