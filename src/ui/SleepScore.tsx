7138ead386b8def6e34d47cd5e7e5b46950c7492
nitoyon <nitoyon@gmail.com>
2025-07-26 23:28:36 +0900

    ResearchCalc: Delete SleepScore.css

--- src/ui/SleepScore.tsx
+++ src/ui/SleepScore.tsx
@@ -1,12 +1,25 @@
-import './SleepScore.css';
 import React from 'react';
+import { styled } from '@mui/system';
 
 interface SleepScoreProps {
-    score: number;
+    score: number,
 }
 
 export default function SleepTime({score}:SleepScoreProps) {
     const s = score;
     const img = `radial-gradient(#ffffff 50%, transparent 51%), conic-gradient(#489eff 0% ${s}%, #dddddd ${s+1}% 100%)`;
-    return <div className="pie" style={{backgroundImage: img}}>{score}</div>;
+    return <StyledPie style={{backgroundImage: img}}>{score}</StyledPie>;
 }
+
+const StyledPie = styled('div')({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '2.4rem',
+    height: '2.4rem',
+    fontSize: '.7rem',
+    fontWeight: 700,
+    borderRadius: '50%',
+    color: '#489eff',
+    backgroundImage: 'radial-gradient(#ffffff 50%, transparent 51%), conic-gradient(#489eff 0% 0%, #dddddd 1% 100%)',
+});
