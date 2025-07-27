6c08d719f6a24416b5b65bfab31cd6809e5d9399
nitoyon <nitoyon@gmail.com>
2025-07-27 13:04:26 +0900

    IV: Add alert button to Amber Canyon in ResearchAreaSelect

--- src/ui/IvCalc/Strength/ResearchAreaSelect.tsx
+++ src/ui/IvCalc/Strength/ResearchAreaSelect.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import fields from '../../../data/fields';
+import MessageDialog from '../../Dialog/MessageDialog';
 import SelectEx from '../../common/SelectEx';
-import { MenuItem } from '@mui/material';
+import { IconButton, MenuItem } from '@mui/material';
+import WarningRoundedIcon from '@mui/icons-material/WarningRounded';
 import { useTranslation } from 'react-i18next';
 
 const ResearchAreaSelect = React.memo(({value, fontSize, onChange}:{
@@ -10,9 +12,16 @@ const ResearchAreaSelect = React.memo(({value, fontSize, onChange}:{
     onChange: (value: number) => void;
 }) => {
     const { t } = useTranslation();
+    const [warningOpen, setWarningOpen] = React.useState(false);
     const onChangeHandler = React.useCallback((fieldIndex: string) => {
         onChange(parseInt(fieldIndex, 10));
     }, [onChange]);
+    const onWarningClick = React.useCallback(() => {
+        setWarningOpen(true);
+    }, []);
+    const onWarningClose = React.useCallback(() => {
+        setWarningOpen(false);
+    }, []);
 
     // prepare field menus
     const fieldMenuItems = [];
@@ -32,12 +41,20 @@ const ResearchAreaSelect = React.memo(({value, fontSize, onChange}:{
         );
     }
 
-    return (
+    const showAlert = value === 6;
+
+    return (<>
         <SelectEx value={value} onChange={onChangeHandler}
             sx={{padding: '0 .2rem', fontSize: fontSize ?? '1rem'}}>
             {fieldMenuItems}
         </SelectEx>
-    );
+        {showAlert && <IconButton size="small" color="warning" style={{padding: '2px'}}
+            onClick={onWarningClick}>
+            <WarningRoundedIcon fontSize={fontSize ? "inherit" : "small"}/>
+        </IconButton>}
+        <MessageDialog open={warningOpen} onClose={onWarningClose}
+            message={t('amber assumption')}/>
+    </>);
 });
 
 export default ResearchAreaSelect;
