89cd27d5300cbac5968eff8c9f0ed88b27dbf4d1
nitoyon <nitoyon@gmail.com>
2025-07-28 00:48:37 +0900

    IV: Add config button to ResearchAreaSelect.tsx when used in box footer

--- src/ui/IvCalc/Strength/ResearchAreaSelect.tsx
+++ src/ui/IvCalc/Strength/ResearchAreaSelect.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import fields from '../../../data/fields';
+import { styled } from '@mui/system';
+import fields, { isExpertField } from '../../../data/fields';
 import MessageDialog from '../../Dialog/MessageDialog';
 import SelectEx from '../../common/SelectEx';
+import FavoriteBerrySelect from './FavoriteBerrySelect';
 import { StrengthParameter } from '../../../util/PokemonStrength';
-import { IconButton, MenuItem } from '@mui/material';
+import {
+    Button, Dialog, DialogActions, DialogContent, IconButton, MenuItem
+} from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
 import WarningRoundedIcon from '@mui/icons-material/WarningRounded';
 import { useTranslation } from 'react-i18next';
 
@@ -15,10 +20,17 @@ const ResearchAreaSelect = React.memo(({showConfigButton, value, fontSize, onCha
 }) => {
     const { t } = useTranslation();
     const [warningOpen, setWarningOpen] = React.useState(false);
+    const [configOpen, setConfigOpen] = React.useState(false);
     const onChangeHandler = React.useCallback((val: string) => {
         const fieldIndex = parseInt(val, 10);
         onChange({...value, fieldIndex});
     }, [onChange, value]);
+    const onConfigClick = React.useCallback(() => {
+        setConfigOpen(true);
+    }, []);
+    const onConfigClose = React.useCallback(() => {
+        setConfigOpen(false);
+    }, []);
     const onWarningClick = React.useCallback(() => {
         setWarningOpen(true);
     }, []);
@@ -44,6 +56,8 @@ const ResearchAreaSelect = React.memo(({showConfigButton, value, fontSize, onCha
         );
     }
 
+    const showConfig = showConfigButton &&
+        (value.fieldIndex === 0 || isExpertField(value.fieldIndex));
     const showAlert = value.fieldIndex === 6;
 
     return (<>
@@ -51,13 +65,52 @@ const ResearchAreaSelect = React.memo(({showConfigButton, value, fontSize, onCha
             sx={{padding: '0 .2rem', fontSize: fontSize ?? '1rem'}}>
             {fieldMenuItems}
         </SelectEx>
+        {showConfig && <IconButton size="small" style={{padding: '2px'}}
+            onClick={onConfigClick}>
+            <SettingsIcon fontSize={fontSize ? "inherit" : "small"}/>
+        </IconButton>}
         {showAlert && <IconButton size="small" color="warning" style={{padding: '2px'}}
             onClick={onWarningClick}>
             <WarningRoundedIcon fontSize={fontSize ? "inherit" : "small"}/>
         </IconButton>}
+        <FavoriteBerrySelectDialog open={configOpen} onClose={onConfigClose}
+            value={value} onChange={onChange}/>
         <MessageDialog open={warningOpen} onClose={onWarningClose}
             message={t('amber assumption')}/>
     </>);
+});
+
+const FavoriteBerrySelectDialog = React.memo(({open, value, onChange, onClose}:{
+    open: boolean,
+    value: StrengthParameter,
+    onChange: (value: StrengthParameter) => void
+    onClose: () => void,
+}) => {
+    const { t } = useTranslation();
+    if (!open) {
+        return <></>;
+    }
+
+    return <Dialog open={open} onClose={onClose}>
+        <StyledFavoriteBerryContent>
+            <FavoriteBerrySelect value={value} onChange={onChange}/>
+        </StyledFavoriteBerryContent>
+        <DialogActions>
+            <Button onClick={onClose}>{t('close')}</Button>
+        </DialogActions>
+    </Dialog>;
+});
+
+const StyledFavoriteBerryContent = styled(DialogContent)({
+    '& > section': {
+        marginTop: '1rem',
+        '&:first-of-type': {
+            marginTop: 0,
+        },
+        '& > label': {
+            display: 'block',
+        },
+    },
 });
 
 export default ResearchAreaSelect;
