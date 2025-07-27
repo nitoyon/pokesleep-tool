29d280e2f1bcc3e64c45009b95882120558893a4
nitoyon <nitoyon@gmail.com>
2025-07-27 23:34:20 +0900

    Add MessageDialog.tsx

---
+++ src/ui/Dialog/MessageDialog.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+    Button, Dialog, DialogActions, DialogContent
+} from '@mui/material';
+import { useTranslation } from 'react-i18next'
+
+const MessageDialog = React.memo(({message, open, onClose}: {
+    onClose: () => void,
+    open: boolean,
+    message: React.ReactNode,
+}) => {
+    const { t } = useTranslation();
+    if (!open) {
+        return <></>;
+    }
+    return (
+        <Dialog open={open} onClose={onClose}>
+            <DialogContent>{message}</DialogContent>
+            <DialogActions>
+                <Button onClick={onClose} autoFocus>{t('close')}</Button>
+            </DialogActions>
+        </Dialog>
+    );
+});
+
+export default MessageDialog;
