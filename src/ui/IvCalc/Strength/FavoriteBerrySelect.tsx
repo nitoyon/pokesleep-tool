cd576c75edee6b32d32ea17fa2f6e16b553b0100
nitoyon <nitoyon@gmail.com>
2025-07-29 00:27:33 +0900

    IV: Extract FavoriteBerrySelect.tsx from StrengthParameterForm.tsx

---
+++ src/ui/IvCalc/Strength/FavoriteBerrySelect.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import TypeSelect from '../TypeSelect';
+import { PokemonType } from '../../../data/pokemons';
+import { StrengthParameter } from '../../../util/PokemonStrength';
+import { useTranslation } from 'react-i18next';
+
+const FavoriteBerrySelect = React.memo(({value, onChange}: {
+    value: StrengthParameter,
+    onChange: (value: StrengthParameter) => void
+}) => {
+    const { t } = useTranslation();
+
+    const onFavoriteBerryChange1 = React.useCallback((type: PokemonType) => {
+        const favoriteType = [value.favoriteType[0] ?? "normal",
+            value.favoriteType[1] ?? "normal",
+            value.favoriteType[2] ?? "normal"];
+        favoriteType[0] = type;
+        onChange({...value, favoriteType});
+    }, [onChange, value]);
+    const onFavoriteBerryChange2 = React.useCallback((type: PokemonType) => {
+        const favoriteType = [value.favoriteType[0] ?? "normal",
+            value.favoriteType[1] ?? "normal",
+            value.favoriteType[2] ?? "normal"];
+        favoriteType[1] = type;
+        onChange({...value, favoriteType});
+    }, [onChange, value]);
+    const onFavoriteBerryChange3 = React.useCallback((type: PokemonType) => {
+        const favoriteType = [value.favoriteType[0] ?? "normal",
+            value.favoriteType[1] ?? "normal",
+            value.favoriteType[2] ?? "normal"];
+        favoriteType[2] = type;
+        onChange({...value, favoriteType});
+    }, [onChange, value]);
+
+    return <section>
+        <label>{t('favorite berry')}:</label>
+        <span>
+            <TypeSelect type={value.favoriteType[0]}
+                onChange={onFavoriteBerryChange1}/>
+            <TypeSelect type={value.favoriteType[1]}
+                onChange={onFavoriteBerryChange2}/>
+            <TypeSelect type={value.favoriteType[2]}
+                onChange={onFavoriteBerryChange3}/>
+        </span>
+    </section>;
+});
+
+export default FavoriteBerrySelect;
