4a3b57e8b6452d0a4fe7edd9056584d141cd7485
nitoyon <nitoyon@gmail.com>
2025-07-29 01:59:51 +0900

    IV: Add UI for selecting main and sub favorite berries

--- src/ui/IvCalc/Strength/FavoriteBerrySelect.tsx
+++ src/ui/IvCalc/Strength/FavoriteBerrySelect.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TypeSelect from '../TypeSelect';
+import { isExpertField } from '../../../data/fields';
 import { PokemonType } from '../../../data/pokemons';
 import { StrengthParameter } from '../../../util/PokemonStrength';
 import { useTranslation } from 'react-i18next';
@@ -9,6 +10,7 @@ const FavoriteBerrySelect = React.memo(({value, onChange}: {
     onChange: (value: StrengthParameter) => void
 }) => {
     const { t } = useTranslation();
+    const expert = isExpertField(value.fieldIndex);
 
     const onFavoriteBerryChange1 = React.useCallback((type: PokemonType) => {
         const favoriteType = [value.favoriteType[0] ?? "normal",
@@ -31,6 +33,27 @@ const FavoriteBerrySelect = React.memo(({value, onChange}: {
         favoriteType[2] = type;
         onChange({...value, favoriteType});
     }, [onChange, value]);
+
+    if (expert === true) {
+        return <>
+            <section>
+                <label>{t('main favorite berry')}:</label>
+                <span>
+                    <TypeSelect type={value.favoriteType[0]} size="small"
+                        onChange={onFavoriteBerryChange1}/>
+                </span>
+            </section>
+            <section>
+                <label>{t('sub favorite berries')}:</label>
+                <span>
+                    <TypeSelect type={value.favoriteType[1]} size="small"
+                        onChange={onFavoriteBerryChange2}/>
+                    <TypeSelect type={value.favoriteType[2]} size="small"
+                        onChange={onFavoriteBerryChange3}/>
+                </span>
+            </section>
+        </>;
+    }
 
     return <section>
         <label>{t('favorite berry')}:</label>
