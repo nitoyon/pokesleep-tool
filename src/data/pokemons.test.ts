3830f9e0d0f9dcc9726967d7f03771e8cb348a88
nitoyon <nitoyon@gmail.com>
2025-08-03 21:03:56 +0900

    IV: Add includeNonFinal arg to getDecendants() function in pokemons.ts

--- src/data/pokemons.test.ts
+++ src/data/pokemons.test.ts
@@ -18,6 +18,28 @@ describe('getDecendants', () => {
         expect(decendants[0].name).toBe("Venusaur");
     });
 
+    test('returns evolved pokemon (non-final)', () => {
+        const p = pokemons.find(x => x.name === "Bulbasaur");
+        if (p === undefined) { fail('p should not be undefined'); }
+
+        const decendants = getDecendants(p, true);
+        expect(decendants.length).toBe(3);
+        expect(decendants[0].name).toBe("Bulbasaur");
+        expect(decendants[1].name).toBe("Ivysaur");
+        expect(decendants[2].name).toBe("Venusaur");
+    });
+
+    test('returns Pikachu decendants (non-final)', () => {
+        const p = pokemons.find(x => x.name === "Pikachu");
+        if (p === undefined) { fail('p should not be undefined'); }
+
+        const decendants = getDecendants(p, true);
+        expect(decendants.length).toBe(3);
+        expect(decendants[0].name).toBe("Pichu");
+        expect(decendants[1].name).toBe("Pikachu");
+        expect(decendants[2].name).toBe("Raichu");
+    });
+
     test('returns Eevee evolution line', () => {
         const p = pokemons.find(x => x.name === "Eevee");
         if (p === undefined) { fail('p should not be undefined'); }
@@ -45,6 +67,18 @@ describe('getDecendants', () => {
         expect(decendants.length).toBe(2);
         expect(decendants[0].name).toBe("Gardevoir");
         expect(decendants[1].name).toBe("Gallade");
+    });
+
+    test('returns Ralts evolution line (non-final)', () => {
+        const p = pokemons.find(x => x.name === "Ralts");
+        if (p === undefined) { fail('p should not be undefined'); }
+
+        const decendants = getDecendants(p, true);
+        expect(decendants.length).toBe(4);
+        expect(decendants[0].name).toBe("Ralts");
+        expect(decendants[1].name).toBe("Kirlia");
+        expect(decendants[2].name).toBe("Gardevoir");
+        expect(decendants[3].name).toBe("Gallade");
     });
 
     test('returns Vulpix (Alola) evolution line', () => {
