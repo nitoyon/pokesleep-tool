5ea9ef68d91d02dc4033fff03d65c82cf9069f9a
nitoyon <nitoyon@gmail.com>
2025-08-31 20:50:02 +0900

    IV: Use SelectEx and dense MenuItems in PeriodSelect

--- src/ui/IvCalc/Strength/PeriodSelect.tsx
+++ src/ui/IvCalc/Strength/PeriodSelect.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import {
-    Divider, MenuItem, Select, SelectChangeEvent,
-} from '@mui/material';
+import { Divider, MenuItem } from '@mui/material';
 import { IvAction } from '../IvState';
+import SelectEx from '../../common/SelectEx';
 import {
     StrengthParameter, whistlePeriod,
 } from '../../../util/PokemonStrength';
@@ -14,28 +13,33 @@ const PeriodSelect = React.memo(({value, dispatch}: {
 }) => {
     const { t } = useTranslation();
 
-    const onPeriodChange = React.useCallback((e: SelectChangeEvent) => {
+    const onPeriodChange = React.useCallback((val: string) => {
         dispatch({type: "changeParameter", payload: {
             parameter: {
                 ...value,
-                period: parseInt(e.target.value, 10),
+                period: parseInt(val, 10),
             },
         }});
     }, [dispatch, value]);
 
-    return <Select variant="standard" onChange={onPeriodChange} value={value.period.toString()}>
-        <MenuItem value={1}>{t('1hour')}</MenuItem>
-        <MenuItem value={3}>{t('3hours')}</MenuItem>
-        <MenuItem value={8}>{t('8hours')}</MenuItem>
-        <MenuItem value={16}>{t('16hours')}</MenuItem>
-        <MenuItem value={24}>{t('1day')}</MenuItem>
-        <MenuItem value={168}>{t('1week')}</MenuItem>
-        <MenuItem value={whistlePeriod}>{t('whistle')}</MenuItem>
-        <Divider/>
-        <MenuItem value={-10}>{t('help x10')}</MenuItem>
-        <MenuItem value={-30}>{t('help x30')}</MenuItem>
-        <MenuItem value={-100}>{t('help x100')}</MenuItem>
-    </Select>;
+    return <SelectEx onChange={onPeriodChange} value={value.period}
+        menuSx={{
+            display: 'grid',
+            gridTemplateColumns: '1fr 1fr',
+        }}
+    >
+        <MenuItem value={1} dense>{t('1hour')}</MenuItem>
+        <MenuItem value={3} dense>{t('3hours')}</MenuItem>
+        <MenuItem value={8} dense>{t('8hours')}</MenuItem>
+        <MenuItem value={16} dense>{t('16hours')}</MenuItem>
+        <MenuItem value={24} dense>{t('1day')}</MenuItem>
+        <MenuItem value={168} dense>{t('1week')}</MenuItem>
+        <Divider style={{gridColumn: '1 / -1'}}/>
+        <MenuItem value={-10} dense>{t('help x10')}</MenuItem>
+        <MenuItem value={-30} dense>{t('help x30')}</MenuItem>
+        <MenuItem value={-100} dense>{t('help x100')}</MenuItem>
+        <MenuItem value={whistlePeriod} dense>{t('whistle')}</MenuItem>
+    </SelectEx>;
 });
 
 export default PeriodSelect;
