3646f2e1a8596b1e5a91e13ac931cd7d83334e56
nitoyon <nitoyon@gmail.com>
2025-09-04 13:27:01 +0900

    IV: Improve berry burst calculation logic

--- src/util/MainSkill.ts
+++ src/util/MainSkill.ts
@@ -182,6 +182,11 @@ export function getSkillSubValue(skill: MainSkillName, skillLevel: number,
         throw new Error(`invalid main skill: ${skill}, ${skillLevel}`);
     }
 
+    if (skill.startsWith("Berry Burst")) {
+        // Get the number of berries gathered from other members
+        return [1, 2, 2, 3, 4, 5][skillLevel - 1];
+    }
+
     if (skill === "Ingredient Magnet S (Plus)") {
         // Get additional ingredient count
         if (firstIngredient === 'coffee') {
@@ -197,6 +202,79 @@ export function getSkillSubValue(skill: MainSkillName, skillLevel: number,
         return [8, 10, 13, 17, 23, 30, 35][skillLevel - 1];
     }
     throw new Error(`This skill doesn’t have a sub-value: ${skill}`);
+}
+
+/**
+ * Returns the additional effect of the main skill for the given
+ * skill and level.
+ * @param skill Name of the main skill.
+ * @param skillLevel Level of the main skill.
+ * @param firstIngredient First ingredient name. Used only for
+ *                        `Ingredient Magnet S (Plus)`.
+ * @returns Additional effect for the skill, or throws if not applicable.
+ */
+export function getLunarBlessingBerryCount(skillLevel: number, species: number):
+{
+    myBerryCount: number,
+    othersBerryCount: number,
+} {
+    switch (skillLevel) {
+        case 1:
+            switch (species) {
+                case 1: return { myBerryCount: 5, othersBerryCount: 1 };
+                case 2: return { myBerryCount: 7, othersBerryCount: 1 };
+                case 3: return { myBerryCount: 9, othersBerryCount: 1 };
+                case 4: return { myBerryCount: 12, othersBerryCount: 1 };
+                case 5: return { myBerryCount: 14, othersBerryCount: 2 };
+            }
+            break;
+        case 2:
+            switch (species) {
+                case 1: return { myBerryCount: 9, othersBerryCount: 1 };
+                case 2: return { myBerryCount: 12, othersBerryCount: 1 };
+                case 3: return { myBerryCount: 15, othersBerryCount: 1 };
+                case 4: return { myBerryCount: 16, othersBerryCount: 2 };
+                case 5: return { myBerryCount: 19, othersBerryCount: 3 };
+            }
+            break;
+        case 3:
+            switch (species) {
+                case 1: return { myBerryCount: 13, othersBerryCount: 1 };
+                case 2: return { myBerryCount: 17, othersBerryCount: 1 };
+                case 3: return { myBerryCount: 18, othersBerryCount: 2 };
+                case 4: return { myBerryCount: 20, othersBerryCount: 3 };
+                case 5: return { myBerryCount: 24, othersBerryCount: 4 };
+            }
+            break;
+        case 4:
+            switch (species) {
+                case 1: return { myBerryCount: 17, othersBerryCount: 1 };
+                case 2: return { myBerryCount: 19, othersBerryCount: 2 };
+                case 3: return { myBerryCount: 25, othersBerryCount: 2 };
+                case 4: return { myBerryCount: 28, othersBerryCount: 3 };
+                case 5: return { myBerryCount: 29, othersBerryCount: 5 };
+            }
+            break;
+        case 5:
+            switch (species) {
+                case 1: return { myBerryCount: 21, othersBerryCount: 1 };
+                case 2: return { myBerryCount: 24, othersBerryCount: 2 };
+                case 3: return { myBerryCount: 27, othersBerryCount: 3 };
+                case 4: return { myBerryCount: 28, othersBerryCount: 5 };
+                case 5: return { myBerryCount: 30, othersBerryCount: 7 };
+            }
+            break;
+        case 6:
+            switch (species) {
+                case 1: return { myBerryCount: 25, othersBerryCount: 1 };
+                case 2: return { myBerryCount: 29, othersBerryCount: 2 };
+                case 3: return { myBerryCount: 30, othersBerryCount: 4 };
+                case 4: return { myBerryCount: 31, othersBerryCount: 6 };
+                case 5: return { myBerryCount: 32, othersBerryCount: 9 };
+            }
+            break;
+    }
+    throw new Error(`Lunar Blessing doesn’t have a sub-value: ${skillLevel}, ${species}`);
 }
 
 /**
