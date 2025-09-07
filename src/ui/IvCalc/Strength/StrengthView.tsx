98a03e8f889a2ea02dea490aebed51272c7f64a4
nitoyon <nitoyon@gmail.com>
2025-09-07 17:27:05 +0900

    IV: Change event label to use SelectEx in StrengthView

--- src/ui/IvCalc/Strength/StrengthView.tsx
+++ src/ui/IvCalc/Strength/StrengthView.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { styled } from '@mui/system';
 import IvState, { IvAction } from '../IvState';
 import AreaControlDialog from './AreaControlDialog';
+import EventConfigDialog from './EventConfigDialog';
 import StrengthBerryIngSkillView from './StrengthBerryIngSkillView';
 import PeriodSelect from './PeriodSelect';
+import SelectEx from '../../common/SelectEx';
 import TextLikeButton from '../../common/TextLikeButton';
 import { getActiveHelpBonus } from '../../../data/events';
 import { isExpertField } from '../../../data/fields';
 import {
     allFavoriteFieldIndex, noFavoriteFieldIndex, StrengthParameter, whistlePeriod,
 } from '../../../util/PokemonStrength';
-import { Button, Collapse } from '@mui/material';
+import { Button, Collapse, IconButton, MenuItem } from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
 import { useTranslation } from 'react-i18next';
 
 const StrengthView = React.memo(({state, dispatch}: {
@@ -19,6 +22,7 @@ const StrengthView = React.memo(({state, dispatch}: {
 }) => {
     const { t } = useTranslation();
     const [areaDialogOpen, setAreaDialogOpen] = React.useState(false);
+    const [eventConfigOpen, setEventConfigOpen] = React.useState(false);
     const pokemonIv = state.pokemonIv;
     const parameter = state.parameter;
     const lowerTabIndex = state.lowerTabIndex;
@@ -36,7 +40,7 @@ const StrengthView = React.memo(({state, dispatch}: {
 
     const onParameterChange = React.useCallback((parameter: StrengthParameter) => {
         dispatch({type: "changeParameter", payload: { parameter }});
-    }, []);
+    }, [dispatch]);
 
     const onCampTicketClick = React.useCallback(() => {
         dispatch({type: "changeParameter", payload: { parameter: {
@@ -44,6 +48,23 @@ const StrengthView = React.memo(({state, dispatch}: {
             isGoodCampTicketSet: !parameter.isGoodCampTicketSet,
         }}});
     }, [dispatch, parameter]);
+
+    const onEventChange = React.useCallback((value: string) => {
+        if (value === 'advanced') {
+            value = 'custom';
+        }
+        dispatch({type: "changeParameter", payload: { parameter: {
+            ...parameter,
+            event: value,
+        }}});
+    }, [dispatch, parameter]);
+
+    const onEventConfigClick = React.useCallback(() => {
+        setEventConfigOpen(true);
+    }, []);
+    const onEventConfigClose = React.useCallback(() => {
+        setEventConfigOpen(false);
+    }, []);
 
     let area: React.ReactNode, fieldBonus: React.ReactNode;
     if (parameter.fieldIndex === allFavoriteFieldIndex) {
@@ -72,7 +93,17 @@ const StrengthView = React.memo(({state, dispatch}: {
     }
     fieldBonus = <small> ({parameter.fieldBonus}%)</small>;
 
-    const isEventScheduled = getActiveHelpBonus(new Date()).length > 0 || parameter.event !== 'none';
+    const activeEvents = getActiveHelpBonus(new Date())
+        .map(x => x.name)
+        .reverse();
+    const isEventScheduled = activeEvents.length > 0 || parameter.event !== 'none';
+    const eventMenuItems = ['none', ...activeEvents, 'custom'].map(x => {
+        let name = x === 'none' ? t('no event') :
+            x === 'custom' ? (t('event') + ': ' + t('events.advanced')) :
+            t(`events.${x}`);
+        return <MenuItem key={x} value={x} dense
+            style={{ textTransform: 'none' }}>{name}</MenuItem>
+    });
 
     return (<div>
         <StrengthBerryIngSkillView pokemonIv={pokemonIv} settings={parameter}
@@ -90,14 +121,22 @@ const StrengthView = React.memo(({state, dispatch}: {
                         <>{t('good camp ticket (short)')}: </>
                         <TextLikeButton onClick={onCampTicketClick} style={{width: '2rem'}}>{t(parameter.isGoodCampTicketSet ? 'on' : 'off')}</TextLikeButton>
                     </li>}
-                    {isEventScheduled && <li>{parameter.event === 'none' ? t('no event') :
-                        parameter.event === 'advanced' ? t('event') + ': ' + t('events.advanced') :
-                        t('events.' + parameter.event)}</li>}
+                    {isEventScheduled && <li>
+                        <SelectEx value={parameter.event} onChange={onEventChange}>
+                            {eventMenuItems}
+                        </SelectEx>
+                        {parameter.event === 'custom' && <IconButton size="small" style={{padding: '2px'}}
+                            onClick={onEventConfigClick}>
+                            <SettingsIcon/>
+                        </IconButton>}
+                    </li>}
                 </ul>
                 <Button onClick={onEditClick} size="small">{t('edit')}</Button>
             </StrengthParameterPreview>
         </Collapse>
         <AreaControlDialog open={areaDialogOpen} onClose={onAreaDialogClose}
+            value={state.parameter} onChange={onParameterChange}/>
+        <EventConfigDialog open={eventConfigOpen} onClose={onEventConfigClose}
             value={state.parameter} onChange={onParameterChange}/>
     </div>);
 });
@@ -129,6 +168,9 @@ const StrengthParameterPreview = styled('div')({
                 marginTop: '-2px',
                 fontSize: '0.8rem',
                 lineHeight: 1.5,
+                '& > svg': {
+                    fontSize: '1rem',
+                },
             },
         },
     },
