b8d05bbd3258fbf4926ff3a335476afbbaadd3a0
nitoyon <nitoyon@gmail.com>
2025-09-28 13:49:12 +0900

    IV: Fix favorite berries for Amber Canyon

--- src/ui/IvCalc/Strength/ResearchAreaSelect.tsx
+++ src/ui/IvCalc/Strength/ResearchAreaSelect.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import fields from '../../../data/fields';
-import MessageDialog from '../../Dialog/MessageDialog';
 import SelectEx from '../../common/SelectEx';
 import AreaControlDialog from './AreaControlDialog';
 import {
@@ -8,7 +7,6 @@ import {
 } from '../../../util/PokemonStrength';
 import { Divider, IconButton, MenuItem } from '@mui/material';
 import SettingsIcon from '@mui/icons-material/Settings';
-import WarningRoundedIcon from '@mui/icons-material/WarningRounded';
 import { useTranslation } from 'react-i18next';
 
 const ResearchAreaSelect = React.memo(({showConfigButton, value, fontSize, onChange}:{
@@ -18,7 +16,6 @@ const ResearchAreaSelect = React.memo(({showConfigButton, value, fontSize, onCha
     onChange: (value: StrengthParameter) => void;
 }) => {
     const { t } = useTranslation();
-    const [warningOpen, setWarningOpen] = React.useState(false);
     const [configOpen, setConfigOpen] = React.useState(false);
     const onChangeHandler = React.useCallback((val: string) => {
         const fieldIndex = parseInt(val, 10);
@@ -29,12 +26,6 @@ const ResearchAreaSelect = React.memo(({showConfigButton, value, fontSize, onCha
     }, []);
     const onConfigClose = React.useCallback(() => {
         setConfigOpen(false);
-    }, []);
-    const onWarningClick = React.useCallback(() => {
-        setWarningOpen(true);
-    }, []);
-    const onWarningClose = React.useCallback(() => {
-        setWarningOpen(false);
     }, []);
 
     // prepare field menus
@@ -61,7 +52,6 @@ const ResearchAreaSelect = React.memo(({showConfigButton, value, fontSize, onCha
         );
     }
 
-    const showAlert = value.fieldIndex === 6 && !showConfigButton;
     return (<>
         <SelectEx value={value.fieldIndex} onChange={onChangeHandler}
             sx={{padding: '0 .2rem', fontSize: fontSize ?? '1rem'}}>
@@ -71,14 +61,8 @@ const ResearchAreaSelect = React.memo(({showConfigButton, value, fontSize, onCha
             onClick={onConfigClick}>
             <SettingsIcon fontSize={fontSize ? "inherit" : "small"}/>
         </IconButton>}
-        {showAlert && <IconButton size="small" color="warning" style={{padding: '2px'}}
-            onClick={onWarningClick}>
-            <WarningRoundedIcon fontSize={fontSize ? "inherit" : "small"}/>
-        </IconButton>}
         <AreaControlDialog open={configOpen} onClose={onConfigClose}
             value={value} onChange={onChange}/>
-        <MessageDialog open={warningOpen} onClose={onWarningClose}
-            message={t('amber assumption')}/>
     </>);
 });
 
