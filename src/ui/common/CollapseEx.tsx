75fda64313d50d76ef9bb6cf8529e0772e8a0e02
nitoyon <nitoyon@gmail.com>
2025-10-03 00:57:54 +0900

    IV: Fix CollapseEx to work correctly
    Following bugs are fixed:
    - On the first render, it animates from height 0 to the current height
    - When children change, it does not animate to the new height

--- src/ui/common/CollapseEx.tsx
+++ src/ui/common/CollapseEx.tsx
@@ -12,6 +12,7 @@ const CollapseEx = React.memo(({show, children}: {
     const ref = React.useRef<HTMLDivElement>(null);
     const [height, setHeight] = React.useState(0);
     const [renderedChildren, setRenderedChildren] = React.useState(children);
+    const isFirstRender = React.useRef(true);
 
     React.useEffect(() => {
         if (show) {
@@ -21,11 +22,15 @@ const CollapseEx = React.memo(({show, children}: {
 
     React.useLayoutEffect(() => {
         setHeight(ref.current ? ref.current.scrollHeight : 0);
-    }, []);
+    }, [renderedChildren, show]);
 
     const styles = useSpring({
         height: show ? height : 0,
         config: { tension: 400, friction: 40 },
+        immediate: isFirstRender.current,
+        onRest: () => {
+            isFirstRender.current = false;
+        },
     });
 
     return <animated.div style={{...styles, overflow: 'hidden'}}>
