8f6dcc34821aefc789e883b718f2c4c07520f127
nitoyon <nitoyon@gmail.com>
2026-09-11 23:51:18 +0900

    IV: Team tab select index stored

--- src/ui/IvCalc/IvState.ts
+++ src/ui/IvCalc/IvState.ts
@@ -185,7 +185,7 @@ function loadInitialIvStateCache(): IvStateCache {
 	if (
 		typeof json.tabIndex === "number" &&
 		json.tabIndex >= 0 &&
-		json.tabIndex <= 2
+		json.tabIndex <= 3
 	) {
 		ret.tabIndex = json.tabIndex;
 	}
