40a80c5bfc3a923caf77a7b397863bb7bca2bb8f
nitoyon <nitoyon@gmail.com>
2026-09-12 18:11:30 +0900

    IV: Add mewtwo warning

--- src/ui/IvCalc/Team/TeamView.tsx
+++ src/ui/IvCalc/Team/TeamView.tsx
@@ -1,6 +1,7 @@
 import { CircularProgress } from "@mui/material";
 import { styled } from "@mui/system";
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { useElementWidth } from "../../common/Hook";
 import { MemberStrengthChart } from "../Chart/MemberStrengthChart";
 import type IvState from "../IvState";
@@ -18,6 +19,7 @@ const TeamView = React.memo(
 		state: IvState;
 		dispatch: (action: IvAction) => void;
 	}) => {
+		const { t } = useTranslation();
 		const [chartWidth, chartRef] = useElementWidth();
 		const activeMembers = React.useMemo(
 			() => state.teamMembers.map((m) => (m?.enabled ? m.item : undefined)),
@@ -35,6 +37,25 @@ const TeamView = React.memo(
 				<div className="chart" ref={chartRef}>
 					<MemberStrengthChart width={chartWidth} results={results.members} />
 				</div>
+				{(state.parameter.event.startsWith("pursue mewtwo") ||
+					state.teamMembers.some(
+						(x) => x?.item?.iv?.pokemonName === "Mewtwo",
+					)) && (
+					<div
+						style={{
+							gridColumn: "1 / -1",
+							border: "1px solid red",
+							background: "#ffeeee",
+							color: "red",
+							fontSize: "0.8rem",
+							borderRadius: "0.5rem",
+							margin: ".2rem .5rem 0",
+							padding: "0 0.3rem",
+						}}
+					>
+						{t("mewtwo warning")}
+					</div>
+				)}
 				<TeamParameterPanel state={state} dispatch={dispatch} />
 				<MemberList state={state} dispatch={dispatch} />
 			</StyledDiv>
