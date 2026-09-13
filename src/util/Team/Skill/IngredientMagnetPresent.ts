edf99467904b0e436477a801314e345c5bae61fe
nitoyon <nitoyon@gmail.com>
2026-09-13 23:59:29 +0900

    IV: Add candy simulation for "Ingredient Magnet S (Present)"

--- src/util/Team/Skill/IngredientMagnetPresent.ts
+++ src/util/Team/Skill/IngredientMagnetPresent.ts
@@ -1,6 +1,24 @@
+import { getSkillSubValue, presentCandyRate } from "../../MainSkill";
+import type { MemberProfile, TeamContext, TeamMember } from "../Types";
 import { IngredientMagnetSkill } from "./IngredientMagnet";
 
 /**
  * Ingredient Magnet S (Present).
  */
-export class IngredientMagnetPresentSkill extends IngredientMagnetSkill {}
+export class IngredientMagnetPresentSkill extends IngredientMagnetSkill {
+	private candyCount = 0;
+
+	initialize(profile: MemberProfile, profiles: MemberProfile[]): void {
+		super.initialize(profile, profiles);
+
+		const baseCount = getSkillSubValue(profile.skillName, profile.skillLevel);
+		this.candyCount = Math.floor(baseCount * this.bonus);
+	}
+
+	apply(member: TeamMember, tapSec: number, sim: TeamContext): void {
+		super.apply(member, tapSec, sim);
+		if (this.rng() < presentCandyRate) {
+			member.progress.skillCandy += this.candyCount;
+		}
+	}
+}
