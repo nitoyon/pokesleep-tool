9b81d592b72bf108670fd1a1bc1b78f1db1f51b9
nitoyon <nitoyon@gmail.com>
2026-09-26 20:35:36 +0900

    IV: Extract BerryCalculatorPanel from TypeInfoDialog

---
+++ src/ui/IvCalc/Panel/BerryCalculatorPanel.tsx
@@ -0,0 +1,146 @@
+import LocalFireDepartmentIcon from "@mui/icons-material/LocalFireDepartment";
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+import { styled } from "@mui/system";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import type { PokemonType } from "../../../data/pokemons";
+import { getBerryStrength } from "../../../util/Berry";
+import type { StrengthParameter } from "../../../util/PokemonStrength";
+import { calcBerryStrengthBonus } from "../../../util/PokemonStrength";
+import { LevelInput } from "../IvForm/LevelControl";
+import type { IvAction } from "../IvState";
+import AreaBonusControl from "../Strength/AreaBonusControl";
+
+const BerryCalculatorPanel = React.memo(
+	({
+		type,
+		level,
+		parameter,
+		dispatch,
+	}: {
+		type: PokemonType;
+		level: number;
+		parameter: StrengthParameter;
+		dispatch: (action: IvAction) => void;
+	}) => {
+		const { t } = useTranslation();
+		const [pokemonLevel, setPokemonLevel] = React.useState(level);
+		const [multiplier, setMultiplier] = React.useState(() =>
+			calcBerryStrengthBonus(type, parameter),
+		);
+
+		const onLevelChange = React.useCallback((value: number) => {
+			setPokemonLevel(value);
+		}, []);
+
+		const onAreaBonusChange = React.useCallback(
+			(fieldBonus: number) => {
+				dispatch({
+					type: "changeParameter",
+					payload: {
+						parameter: { ...parameter, fieldBonus },
+					},
+				});
+			},
+			[dispatch, parameter],
+		);
+
+		const onMultiplierChange = React.useCallback(
+			(_event: React.MouseEvent<HTMLElement>, value: string) => {
+				if (value === null) {
+					return;
+				}
+				setMultiplier(parseFloat(value));
+			},
+			[],
+		);
+
+		React.useEffect(() => {
+			setPokemonLevel(level);
+			setMultiplier(calcBerryStrengthBonus(type, parameter));
+		}, [level, type, parameter]);
+
+		const strength = getBerryStrength(
+			type,
+			pokemonLevel,
+			parameter.fieldBonus,
+			multiplier,
+		);
+
+		return (
+			<StyledPanel>
+				<section>
+					<span className="lbl" style={{ paddingRight: ".5rem" }}>
+						<strong>{t("berry strength calculator")}:</strong>
+					</span>
+					<div>
+						<LocalFireDepartmentIcon sx={{ color: "#ff944b" }} />
+						{strength}
+					</div>
+				</section>
+				<section>
+					<span className="lbl">{t("pokemon level")}:</span>
+					<LevelInput
+						showSlider
+						value={pokemonLevel}
+						onChange={onLevelChange}
+					/>
+				</section>
+				<section>
+					<span className="lbl">{t("area bonus")}:</span>
+					<AreaBonusControl
+						value={parameter.fieldBonus}
+						onChange={onAreaBonusChange}
+					/>
+				</section>
+				<section>
+					<span className="lbl">{t("multiplier")}:</span>
+					<ToggleButtonGroup
+						value={multiplier}
+						exclusive
+						onChange={onMultiplierChange}
+					>
+						<ToggleButton value={1}>×1</ToggleButton>
+						<ToggleButton value={2}>×2</ToggleButton>
+						<ToggleButton value={2.4}>×2.4</ToggleButton>
+					</ToggleButtonGroup>
+				</section>
+			</StyledPanel>
+		);
+	},
+);
+
+const StyledPanel = styled("div")({
+	background: "#eee",
+	borderRadius: "0.9rem",
+	marginTop: "1rem",
+	padding: "0.4rem 0.5rem",
+	"& > section": {
+		display: "flex",
+		flex: "0 auto",
+		alignItems: "start",
+		marginTop: "0.5rem",
+		"&:first-of-type": {
+			marginTop: 0,
+		},
+		"& > span.lbl": {
+			marginRight: "auto",
+			fontSize: "0.9rem",
+		},
+		"& > div": {
+			marginRight: 0,
+			display: "flex",
+			alignItems: "center",
+			lineHeight: 1.2,
+			"& > div > input": {
+				fontSize: "0.9rem",
+				width: "3rem",
+			},
+		},
+		"& > div.MuiToggleButtonGroup-root > button": {
+			padding: "4px 10px",
+		},
+	},
+});
+
+export default BerryCalculatorPanel;
